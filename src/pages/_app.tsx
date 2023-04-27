aecef2eb32b744569e8bcb1ef7f76f32acf7fc05
bohongu <jaehong2782@naver.com>
2023-04-27 22:02:34 +0900

    feat: Tailwind CSS 스타일 지정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
