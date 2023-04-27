a92f1f939b1e0edd918d56d29c59d9cf4394cd8f
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-04-27 09:21:17 +0900

    chore: Recoil, TanStack Query, Axios 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
