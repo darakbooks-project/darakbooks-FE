28f86ac31630eb1c2dfe310e1d6da9018652cb95
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-04-30 22:18:53 +0900

    feat: 버튼 size, color 설정 기능 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,12 +1,16 @@
 interface Props {
   text: string;
+  size: 'small' | 'medium' | 'large';
+  color: 'blue' | 'gray';
   onButtonClick?: React.MouseEventHandler<HTMLDivElement>;
 }
 
-const Button = ({ text, onButtonClick }: Props) => {
+const Button = ({ text, size, color, onButtonClick }: Props) => {
   return (
-    <div className='w-32' onClick={onButtonClick}>
-      <button className='w-full h-12 px-[6px] py-[8px] rounded border-0 outline-0 bg-violet-500 hover:bg-violet-600 box-border cursor-pointer ease-in duration-300'>
+    <div className={`${buttonSize[size]}`} onClick={onButtonClick}>
+      <button
+        className={`${buttonColor[color]} w-full h-full px-[6px] py-[8px] rounded border-0 outline-0 box-border cursor-pointer ease-in duration-300`}
+      >
         {text}
       </button>
     </div>
@@ -14,3 +18,22 @@ const Button = ({ text, onButtonClick }: Props) => {
 };
 
 export default Button;
+
+interface buttonSizeType {
+  [key: string]: string;
+}
+
+interface buttonColorType {
+  [key: string]: string;
+}
+
+const buttonSize: buttonSizeType = {
+  small: 'w-20 h-8 text-xs',
+  medium: 'w-24 h-10 text-base',
+  large: 'w-32 h-12 text-lg',
+};
+
+const buttonColor: buttonColorType = {
+  blue: 'bg-violet-500 hover:bg-violet-600 text-white',
+  gray: 'bg-gray-300 hover:bg-gray-400 text-black',
+};
