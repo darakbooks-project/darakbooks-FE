1011d0283090896778b93282a9f579f752ec6431
JaeHong Jeong <91203029+bohongu@users.noreply.github.com>
2023-05-01 13:26:14 +0900

    chore: react-query hydration 설정변경 (#20)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,24 @@
 import '@/styles/globals.css';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import React from 'react';
 import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+  const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Hydrate>
     </QueryClientProvider>
   );
 }
