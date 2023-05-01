51ea7762055ebce287b4230b57f3903672d5d929
bohongu <jaehong2782@naver.com>
2023-05-01 16:41:53 +0900

    feat: 도서 기록 페이지 이미지 업로드, 미리보기 로직 구성

--- src/pages/book/record/index.tsx
+++ src/pages/book/record/index.tsx
@@ -1,10 +1,42 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
+
+import { MAX_FILE_SIZE } from '@/constants/file';
+
+interface postImageProps {
+  id: string;
+  url: string;
+}
 
 const BookRecordPage = () => {
+  const [postImage, setPostImage] = useState<postImageProps>();
+
   const {
     query: { bid },
   } = useRouter();
+
+  const postBookRecordImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.currentTarget;
+    const formData = new FormData();
+    if (!files) {
+      return;
+    }
+    if (files[0].size > MAX_FILE_SIZE) {
+      alert('업로드 가능한 최대 용량은 3MB입니다.');
+      return;
+    } else {
+      formData.append('image', files[0]);
+      {
+        /*
+       1. react-query mutate를 이용 post
+       2. 성공 시 이미지 id와 url return 받기
+       3. url로 이미지 미리보기 구현
+       */
+      }
+      console.log(formData.get('image'));
+    }
+  };
 
   return (
     <div className='h-screen p-5 border-2 border-red-500'>
@@ -18,13 +50,31 @@ const BookRecordPage = () => {
         </div>
       </div>
       <form className='h-4/5 border-basic flex flex-col items-center space-y-5'>
-        <label
-          htmlFor='book-image'
-          className='flex border-basic w-full h-1/4 mt-5 justify-center items-center'
-        >
-          이미지를 선택해주세요
-        </label>
-        <input type='file' id='book-image' className='hidden' />
+        {postImage ? (
+          <Image
+            src={postImage.url}
+            alt='image_preview'
+            width={150}
+            height={200}
+          />
+        ) : (
+          <>
+            <label
+              htmlFor='book-image'
+              className='flex border-basic w-full h-1/4 mt-5 justify-center items-center'
+            >
+              이미지를 선택해주세요 (기본으로 책표지 이미지 제공. 쿼리스트링
+              활용 postImage default 값에 url 넣어주면 될 듯)
+            </label>
+            <input
+              type='file'
+              id='book-image'
+              className='hidden'
+              onChange={postBookRecordImage}
+            />
+          </>
+        )}
+
         <textarea className='border-basic w-full resize-none h-2/4'></textarea>
         <div className='border-basic w-full'>태그들</div>
         <div className='flex justify-end w-full'>
