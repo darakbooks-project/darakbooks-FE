cf24823d5b5cd9c6e3ec06154a53e3c514d62ab8
bohongu <jaehong2782@naver.com>
2023-05-01 15:14:29 +0900

    feat: 도서 기록 페이지 레이아웃 제작

--- src/pages/book/record/index.tsx
+++ src/pages/book/record/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 const BookRecordPage = () => {
-  return <div>도서 기록 페이지</div>;
+  return (
+    <div className='h-screen p-5 border-2 border-red-500'>
+      <div className='h-1/5 border-basic flex flex-row '>
+        <div className='w-1/3 border-basic'>IMAGE</div>
+        <div className='flex flex-col justify-around pl-5'>
+          <button className='border-basic'>책 선택</button>
+          <div className='border-basic'>읽은날짜</div>
+        </div>
+      </div>
+      <form className='h-4/5 border-basic flex flex-col items-center space-y-5'>
+        <label
+          htmlFor='book-image'
+          className='flex border-basic w-full h-1/4 mt-5 justify-center items-center'
+        >
+          이미지를 선택해주세요
+        </label>
+        <input type='file' id='book-image' className='hidden' />
+        <textarea className='border-basic w-full resize-none h-2/4'></textarea>
+        <div className='border-basic w-full'>태그들</div>
+        <div className='flex justify-end w-full'>
+          <button className='border-basic'>비공개</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default BookRecordPage;
