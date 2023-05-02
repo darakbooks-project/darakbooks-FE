7404582a67d79b0a1afc8e242613a2b2f0ff7c04
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-02 22:31:35 +0900

    feat: Toggle 컴포넌트 마크업

---
+++ src/components/common/Toggle/index.tsx
@@ -0,0 +1,48 @@
+const Toggle = () => {
+  return (
+    <>
+      <label className='inline-block cursor-pointer select-none'>
+        <input type='checkbox' />
+        <div className='w-[64px] h-[30px] p-[2px] rounded-[15px] box-border	bg-gray-300 ease-out duration-200' />
+      </label>
+      <style jsx>
+        {`
+          div::after {
+            content: '';
+            position: relative;
+            left: 0;
+            display: block;
+            width: 26px;
+            height: 26px;
+            border-radius: 50%;
+            background-color: white;
+            transition: left 0.2s ease-out;
+          }
+
+          input {
+            display: none;
+
+            &:checked + div {
+              background: blue;
+            }
+
+            &:checked + div:after {
+              left: calc(100% - 26px);
+            }
+
+            &:disabled + div {
+              opacity: 0.7;
+              cursor: not-allowed;
+
+              &:after {
+                opacity: 0.7;
+              }
+            }
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default Toggle;
