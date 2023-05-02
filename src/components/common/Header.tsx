794b04eefffe004f4c63b2b0d15b6fa2024e29d3
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-02 21:30:51 +0900

    feat: header 공통 컴포넌트 구현 (#27)
    * feat: Header 컴포넌트 마크업
    
    * feat: props type 설정
    
    * feat: 버튼 클릭 시 뒤로 가기

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+interface Props {
+  title: string;
+}
+
+const Header = ({ title }: Props) => {
+  const router = useRouter();
+
+  return (
+    <div className='flex my-[20px] items-center'>
+      <Image
+        onClick={() => router.back()}
+        className='mr-[15px] cursor-pointer'
+        src='./images/back-button.svg'
+        width={20}
+        height={20}
+        alt='뒤로 가기 버튼입니다'
+      />
+      <div className='text-xl'>{title}</div>
+    </div>
+  );
+};
+
+export default Header;
