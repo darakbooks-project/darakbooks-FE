816c7265d8870122058d706e725b9e62b79cc20f
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-02 20:11:55 +0900

    feat: props type 설정

--- src/components/common/BookShelfPreviw.tsx
+++ src/components/common/BookShelfPreviw.tsx
@@ -1,6 +1,10 @@
 import BookImage from './BookImage';
 
-const BookShelfPreview = ({ srcArr }) => {
+interface Props {
+  srcArr: string[];
+}
+
+const BookShelfPreview = ({ srcArr }: Props) => {
   return (
     <div className='w-[100%] h-[130px] flex justify-evenly items-center bg-gray-400 cursor-pointer'>
       {srcArr.map((src: string) => {
