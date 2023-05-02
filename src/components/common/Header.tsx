651eeb0a440242d4360506b3167bec5fdea1f6f8
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-02 18:07:04 +0900

    feat: Header 컴포넌트 마크업

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+const Header = ({ title }) => {
+  return (
+    <div className='flex my-[20px]'>
+      <Image
+        className='mr-[15px]'
+        src='./images/back-button.svg'
+        width={20}
+        height={20}
+        alt='뒤로 가기 버튼입니다'
+      />
+      <div className='text-xl'>{title}</div>
+    </div>
+  );
+};
+
+export default Header;
