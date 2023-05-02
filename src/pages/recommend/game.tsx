d94b39e9a9e3b9e910428bde626f3b2abeab8c3b
bohongu <jaehong2782@naver.com>
2023-05-02 20:23:41 +0900

    feat: 추천 라우트

---
+++ src/pages/recommend/game.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecommendGamePage = () => {
+  return <div>추천 페이지(게임)</div>;
+};
+
+export default RecommendGamePage;
---
+++ src/pages/recommend/random.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecommendRandomPage = () => {
+  return <div>추천 페이지(랜덤)</div>;
+};
+
+export default RecommendRandomPage;
