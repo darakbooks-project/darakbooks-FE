65592add63775a71d4bd8f175b8ea56bc50c527c
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-02 15:30:15 +0900

    feat: 아무것도 입력하지 않고 검색 시 오류 메시지 render

--- src/components/common/SearchInput.tsx
+++ src/components/common/SearchInput.tsx
@@ -7,39 +7,51 @@ interface Props {
 
 const SearchInput = ({ getSearchData }: Props) => {
   const [inputData, setInputData] = useState('');
+  const [isError, setIsError] = useState(false);
 
   const updateInputData = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputData(event.target.value);
   };
 
   const onClickSearchButton = () => {
+    if (!inputData) {
+      setIsError(true);
+      return;
+    }
+
+    if (isError) {
+      setIsError(false);
+    }
     getSearchData(inputData);
   };
 
   return (
-    <form
-      className='flex w-[100%] h-[50px] flex-row-reverse'
-      onSubmit={(e) => e.preventDefault()}
-    >
-      <input
-        className='w-[85%] border border-solid border-black rounded-r-xl outline-none pl-2'
-        type='text'
-        placeholder='검색어를 입력해주세요'
-        onChange={updateInputData}
-      />
-      <button
-        type='submit'
-        className='w-[15%] border border-solid border-black bg-gray-300 flex justify-center items-center rounded-l-xl border-r-0'
-        onClick={onClickSearchButton}
+    <div>
+      <form
+        className='flex w-[100%] h-[50px] flex-row-reverse'
+        onSubmit={(e) => e.preventDefault()}
       >
-        <Image
-          src='./images/search.svg'
-          width={35}
-          height={35}
-          alt='검색 아이콘'
+        <input
+          className='w-[85%] border border-solid border-black rounded-r-xl outline-none pl-2'
+          type='text'
+          placeholder='검색어를 입력해주세요'
+          onChange={updateInputData}
         />
-      </button>
-    </form>
+        <button
+          type='submit'
+          className='w-[15%] border border-solid border-black bg-gray-300 flex justify-center items-center rounded-l-xl border-r-0'
+          onClick={onClickSearchButton}
+        >
+          <Image
+            src='./images/search.svg'
+            width={35}
+            height={35}
+            alt='검색 아이콘'
+          />
+        </button>
+      </form>
+      <div>{isError && '한글자 이상의 검색어를 입력해주세요'}</div>
+    </div>
   );
 };
 
