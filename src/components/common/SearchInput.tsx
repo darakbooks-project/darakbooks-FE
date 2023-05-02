7d22503377ceacb0332e2a289f9c934e52c4e5dd
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-02 14:34:33 +0900

    feat: input에 입력 시 input 내용으로 state 수정

--- src/components/common/SearchInput.tsx
+++ src/components/common/SearchInput.tsx
@@ -2,12 +2,19 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 const SearchInput = () => {
+  const [inputData, setInputData] = useState('');
+
+  const updateInputData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(event.target.value);
+  };
+
   return (
     <div className='flex w-[100%] h-[50px] flex-row-reverse'>
       <input
         className='w-[85%] border border-solid border-black rounded-r-xl outline-none pl-2'
         type='text'
         placeholder='검색어를 입력해주세요'
+        onChange={updateInputData}
       />
       <button className='w-[15%] border border-solid border-black bg-gray-300 flex justify-center items-center rounded-l-xl border-r-0'>
         <Image
