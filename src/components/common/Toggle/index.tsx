47ed13a2aabb3e446c584fad0cb737b46eeb1689
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-03 20:06:33 +0900

    feat: toggle 버튼 클릭 시 props로 전달 받은 함수 호출

--- src/components/common/Toggle/index.tsx
+++ src/components/common/Toggle/index.tsx
@@ -1,14 +1,35 @@
+import useToggle from './useToggle';
+
 interface ToggleProps {
   name: string;
-  disabled: true;
+  on: boolean;
+  disabled?: boolean;
   onChange(): void;
 }
 
-const Toggle = ({ name, disabled, onChange }) => {
+const Toggle = ({
+  name,
+  on = false,
+  disabled = false,
+  onChange,
+}: ToggleProps) => {
+  const { isCheck, toggle } = useToggle(on);
+
+  const handleChange = () => {
+    toggle();
+    onChange();
+  };
+
   return (
     <>
       <label className='inline-block cursor-pointer select-none'>
-        <input type='checkbox' name={name} disabled={disabled} />
+        <input
+          type='checkbox'
+          name={name}
+          checked={isCheck}
+          disabled={disabled}
+          onChange={handleChange}
+        />
         <div className='w-[64px] h-[30px] p-[2px] rounded-[15px] box-border	bg-gray-300 ease-out duration-200' />
       </label>
       <style jsx>
