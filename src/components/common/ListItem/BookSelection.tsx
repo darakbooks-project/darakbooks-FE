422d15a0b490c3ba7cd1e8f993c16cf5efd7acc6
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-03 21:25:25 +0900

    feat: props 설정

--- src/components/common/ListItem/BookSelection.tsx
+++ src/components/common/ListItem/BookSelection.tsx
@@ -1,20 +1,20 @@
 import BookImage from '../BookImage';
 import Button from '../Button';
 
-const BookSelection = () => {
+const BookSelection = ({ src, text, imageSize, buttonSize }) => {
   return (
     <div className='w-[100%] flex justify-between items-center bg-yellow-600 px-[20px] py-[10px]'>
       <div className='flex items-center'>
         <BookImage
           lazy={true}
-          src='https://image.yes24.com/momo/TopCate420/MidCate010/41997675(1).jpg'
+          src={src}
           placeholder='스켈레톤'
           alt='책 선택 리스트 아이템의 사진 입니다.'
-          feed='not-feed-small'
+          feed={imageSize}
         />
-        <div className='ml-[15px] font-bold'>미움 받을 용기</div>
+        <div className='ml-[15px] font-bold'>{text}</div>
       </div>
-      <Button size='small' color='gray'>
+      <Button size={buttonSize} color='gray'>
         선택
       </Button>
     </div>
