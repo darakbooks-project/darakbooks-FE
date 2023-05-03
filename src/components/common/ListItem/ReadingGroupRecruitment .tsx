b1d8b8099256dbca45c4e3a46e6acfd2e8a5a8d7
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-03 23:39:09 +0900

    feat: props 설정 및 props type 설정

--- src/components/common/ListItem/ReadingGroupRecruitment .tsx
+++ src/components/common/ListItem/ReadingGroupRecruitment .tsx
@@ -3,7 +3,27 @@ import { useState } from 'react';
 
 import BookImage from '../BookImage';
 
-const ReadingGroupRecruitment = () => {
+interface GruopProps {
+  name: string;
+  deadline: string;
+  type: '온라인' | '오프라인';
+  day: '월' | '화' | '수' | '목' | '금' | '토' | '일';
+  region: string;
+  isRecruitment: boolean;
+  recruitmentNumber: number;
+}
+
+interface ReadingGroupRecruitmentProps {
+  src: string;
+  group: GruopProps;
+  onClickDeleteButton(): void;
+}
+
+const ReadingGroupRecruitment = ({
+  src,
+  group,
+  onClickDeleteButton,
+}: ReadingGroupRecruitmentProps) => {
   const [isSettingButtonOpen, setIsSettingButtonOpen] = useState(false);
 
   const clickSettingButton = () => {
@@ -14,22 +34,22 @@ const ReadingGroupRecruitment = () => {
     <div className='w-[100%] flex items-center bg-yellow-600 px-[20px] py-[15px] relative'>
       <BookImage
         lazy={true}
-        src='https://cdn-icons-png.flaticon.com/512/1436/1436701.png'
+        src={src}
         placeholder='스켈레톤'
         alt='책 선택 리스트 아이템의 사진 입니다.'
         feed='not-feed-small'
       />
       <div className='font-bold text-[15px] ml-[20px]'>
-        <span className='text-xl'>책 모임</span>
+        <span className='text-xl'>{group.name}</span>
         <div className='space-x-2 my-[2px]'>
-          <span>~2023.05.03</span>
-          <span>오프라인</span>
-          <span>금요일</span>
-          <span>울산</span>
+          <span>{group.deadline}</span>
+          <span>{group.type}</span>
+          <span>{group.day}요일</span>
+          <span>{group.region}</span>
         </div>
         <div className='space-x-2'>
-          <span>모집 중</span>
-          <span>2명</span>
+          <span>{group.isRecruitment ? '모집 중' : '모집 완료'}</span>
+          <span>{group.isRecruitment && `${group.recruitmentNumber}명`}</span>
         </div>
       </div>
       <Image
