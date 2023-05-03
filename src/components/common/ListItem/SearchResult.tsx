564aac494685821cadf47c481d1e7b0f1b8503d0
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-03 21:41:07 +0900

    refactor: text 위치 문제로 인한 레이아웃 수정

--- src/components/common/ListItem/SearchResult.tsx
+++ src/components/common/ListItem/SearchResult.tsx
@@ -2,15 +2,17 @@ import BookImage from '../BookImage';
 
 const SearchResult = ({ src, imageSize, title, author, publisher }) => {
   return (
-    <div className='w-[100%] flex justify-between items-center bg-yellow-600 px-[20px] py-[10px]'>
-      <BookImage
-        lazy={true}
-        src={src}
-        placeholder='스켈레톤'
-        alt='책 선택 리스트 아이템의 사진 입니다.'
-        feed={imageSize}
-      />
-      <div className='flex flex-col font-bold text-[15px]'>
+    <div className='w-[100%] flex items-center bg-yellow-600 px-[20px] py-[10px]'>
+      <div className='w-[50%]'>
+        <BookImage
+          lazy={true}
+          src={src}
+          placeholder='스켈레톤'
+          alt='책 선택 리스트 아이템의 사진 입니다.'
+          feed={imageSize}
+        />
+      </div>
+      <div className='w-[50%] flex flex-col font-bold text-[15px]'>
         <span>{title}</span>
         <span>{author}</span>
         <span>{publisher}</span>
