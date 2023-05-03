b35cf68466a2c090023fbdc78cd45ebbded1fef9
bohongu <jaehong2782@naver.com>
2023-05-03 16:11:33 +0900

    refactor: Semantic Tag 적용

--- src/pages/book/detail/index.tsx
+++ src/pages/book/detail/index.tsx
@@ -5,25 +5,23 @@ const BookDetailPage = () => {
   const router = useRouter();
   return (
     <div className='h-screen p-5 border-2 border-red-500'>
-      <div className='h-full flex flex-col gap-5'>
-        <div className='min-h-[10rem] flex'>
-          <div className='w-[30%] border-basic'>이미지</div>
+      <main className='h-full flex flex-col gap-5'>
+        <section className='min-h-[10rem] flex'>
+          <figure className='w-[30%] border-basic'>이미지</figure>
           <div className='w-[70%] flex flex-col pl-1'>
-            <div className='flex justify-between mb-2'>
-              <div className='flex'>
-                <h1 className='border-basic'>책명</h1>
-                <div className='border-basic'>저자</div>
-              </div>
-              <button className='border-basic'>책장에 담기</button>
+            <div className='flex  mb-2'>
+              <h1 className='border-basic'>책명</h1>
+              <h2 className='border-basic'>저자</h2>
             </div>
-            <div className='h-full text-xs border-basic'>
+            <article className='h-full text-xs border-basic'>
               책내용 사이즈보고 ... 잘라내기 책내용 사이즈보고 ... 잘라내기
               책내용 사이즈보고 ... 잘라내기 책내용 사이즈보고 ... 잘라내기
               책내용 사이즈보고 ... 잘라내기
-            </div>
+            </article>
           </div>
-        </div>
-        <div className='border-basic flex justify-end'>
+        </section>
+        <section className='border-basic flex justify-end'>
+          <button className='border-basic'>책장에 담기</button>
           <button
             className='border-basic'
             onClick={() => {
@@ -35,11 +33,11 @@ const BookDetailPage = () => {
           >
             독서기록
           </button>
-        </div>
-        <div className='border-basic'>
+        </section>
+        <section className='border-basic'>
           <div>태그들</div>
-        </div>
-        <div className='grid grid-cols-3 4 gap-1 overflow-scroll'>
+        </section>
+        <section className='grid grid-cols-3 4 gap-1 overflow-scroll'>
           <div className='min-h-[8rem] border-basic'>1</div>
           <div className='min-h-[8rem] border-basic'>1</div>
           <div className='min-h-[8rem] border-basic'>1</div>
@@ -55,8 +53,8 @@ const BookDetailPage = () => {
           <div className='min-h-[8rem] border-basic'>1</div>
           <div className='min-h-[8rem] border-basic'>1</div>
           <div className='min-h-[8rem] border-basic'>1</div>
-        </div>
-      </div>
+        </section>
+      </main>
     </div>
   );
 };
