c80380193b5b34fd0e20cd53edb71e61d6a8f0a2
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-04 14:04:29 +0900

    feat: 배열 객체의 데이터를 바인딩

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -1,12 +1,46 @@
 const BottomNav = () => {
+  const navItemPropertyArr = [
+    {
+      path: '/',
+      text: '메인',
+      // by 민형, 디자인에서 이미지 아이콘 정해지면 아이콘 url 설정_230504
+      src: '',
+    },
+    {
+      path: '/book/search',
+      text: '검색',
+      src: '',
+    },
+    {
+      path: '/book/record',
+      text: '기록',
+      src: '',
+      onClick: () => console.log('기록 페이지로 가시려면 로그인이 필요합니다.'),
+    },
+    {
+      path: '/recommend/random',
+      text: '추천',
+      src: '',
+    },
+    {
+      path: '/recruit’',
+      text: '모집',
+      src: '',
+    },
+  ];
+
   return (
     <nav className='fixed left-[50%] bottom-0 translate-x-[-50%] w-full h-[4rem] bg-yellow-200 max-w-3xl flex items-centerr'>
       <div className='flex justify-around items-center w-full'>
-        <div className='cursor-pointer w-14 text-center'>메인</div>
-        <div className='cursor-pointer w-14 text-center'>검색</div>
-        <div className='cursor-pointer w-14 text-center'>기록</div>
-        <div className='cursor-pointer w-14 text-center'>추천</div>
-        <div className='cursor-pointer w-14 text-center'>모집</div>
+        {navItemPropertyArr.map((button, index) => {
+          const { text } = button;
+
+          return (
+            <div key={index} className='cursor-pointer w-14 text-center'>
+              {text}
+            </div>
+          );
+        })}
       </div>
     </nav>
   );
