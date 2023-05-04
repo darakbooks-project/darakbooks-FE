86794fd833956a739e51f45385bc4544ad8abda0
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-04 14:32:22 +0900

    feat: 객체 데이터 type 설정

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -1,8 +1,15 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+interface ButtonType {
+  src: string; // by 민형, 디자인에서 이미지 아이콘 정해지면 클릭여부에 따라 아이콘 설정하는 함수 type으로 변경_230504
+  text: string;
+  path: string;
+  onClick?: () => void;
+}
+
 const BottomNav = () => {
-  const navItemPropertyArr = [
+  const navItemPropertyArr: ButtonType[] = [
     {
       path: '/',
       text: '메인',
@@ -18,6 +25,7 @@ const BottomNav = () => {
       path: '/book/record',
       text: '기록',
       src: '',
+      // by 민형, 비 로그인 유저가 클릭 시 로그인을 할 것인지 물어보는 작업을 수행하는 함수로 변경_230504
       onClick: () => console.log('기록 페이지로 가시려면 로그인이 필요합니다.'),
     },
     {
@@ -38,7 +46,7 @@ const BottomNav = () => {
   return (
     <nav className='fixed left-[50%] bottom-0 translate-x-[-50%] w-full h-[4rem] bg-yellow-200 max-w-3xl flex items-centerr'>
       <div className='flex justify-around items-center w-full'>
-        {navItemPropertyArr.map((button, index) => {
+        {navItemPropertyArr.map((button: ButtonType, index: number) => {
           const { path, text } = button;
           const isClicked = path === pathname;
 
