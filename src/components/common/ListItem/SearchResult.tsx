32594a97d07a72a7ea56d3564274a66dd3663d0f
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-05 00:25:42 +0900

    feat: 리스트 아이템 관련 공통 컴포넌트 구현 (#35)
    * feat: 도서 선택 리스트 아이템 컴포넌트 마크업
    
    * feat: props 설정
    
    * feat: props type 설정
    
    * feat: 검색 결과 리스트 아이템 컴포넌트 마크업
    
    * feat: props 설정
    
    * refactor: text 위치 문제로 인한 레이아웃 수정
    
    * feat: props type 설정
    
    * feat: 독서 모임 모집 리스트 아이템 마크업
    
    * feat: 설정 버튼 마크업
    
    * feat: 설정 버튼의 세부 버튼 toggle 기능 구현
    
    * feat: props 설정 및 props type 설정
    
    * feat: 수정, 삭제 기능 구현
    
    * refactor: 책 선택 컴포넌트 props명 수정
    
    * refactor: style 수정

---
+++ src/components/common/ListItem/SearchResult.tsx
@@ -0,0 +1,38 @@
+import BookImage from '../BookImage';
+
+interface SearchResultProps {
+  src: string;
+  imageSize: string;
+  title: string;
+  author: string;
+  publisher: string;
+}
+
+const SearchResult = ({
+  src,
+  imageSize,
+  title,
+  author,
+  publisher,
+}: SearchResultProps) => {
+  return (
+    <div className='w-[100%] flex items-center bg-yellow-500 px-[20px] py-[15px] cursor-pointer'>
+      <div className='w-[50%]'>
+        <BookImage
+          lazy={true}
+          src={src}
+          placeholder='스켈레톤'
+          alt='책 선택 리스트 아이템의 사진 입니다.'
+          feed={imageSize}
+        />
+      </div>
+      <div className='w-[50%] flex flex-col font-bold text-[15px]'>
+        <span>{title}</span>
+        <span>{author}</span>
+        <span>{publisher}</span>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResult;
