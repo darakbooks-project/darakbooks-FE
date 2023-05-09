bb0b331850e13de74d82c989e6dbbd75951ace8d
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-09 20:25:07 +0900

    feat: modal wrapper 구현

---
+++ src/components/common/Modal/Wrapper.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+
+interface ModalWrapperProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+}
+
+const ModalWrapper = ({ children, isOpen }: ModalWrapperProps) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'initial';
+    }
+    return () => {
+      document.body.style.overflow = 'initial';
+    };
+  }, [isOpen]);
+
+  return (
+    <div
+      className={`fixed top-0 left-0 z-50 w-full min-h-screen bg-black bg-opacity-60 display: ${
+        isOpen ? 'block' : 'none'
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default ModalWrapper;
