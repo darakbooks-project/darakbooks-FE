10bcf72f394d6b37e849dbe23f503fa0687447dc
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-09 14:44:04 +0900

    feat: 로그인 페이지 마크업

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import Image from 'next/image';
 
 const LoginPage = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <div className='flex flex-col items-center'>
+      <Image
+        src='./images/login-image.svg'
+        width={200}
+        height={400}
+        alt='로그인 페이지 사진'
+      />
+      <div>서비스 이름</div>
+      <div>서비스 한 줄 설명</div>
+      <button>카카오 로그인</button>
+      <footer>© 서비스 이름. All rights reversed.</footer>
+    </div>
+  );
 };
 
 export default LoginPage;
