d7a589a8dc2b3c496767e5ad5f9fbd928d426718
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-10 15:48:28 +0900

    feat: 모달 공통 컴포넌트 (#50)
    * feat: 로그인 페이지 마크업
    
    * feat: 상수 설정
    
    * feat: 로그인 버튼 컴포넌트 제작
    
    * feat: 리 다이렉트 페이지
    
    * feat: 로그인 요청 API
    
    * feat: 토큰 및 모달 open 여부 전역 상태 관리
    
    * feat: auth 관련 hook
    
    * feat: 로그인 한 유저만 독서 기록 페이지로 이동
    
    * feat: modal wrapper 구현
    
    * feat: modal 구현
    
    * refactor: display 속성 값 잘못 입력한 것 수정

---
+++ src/components/common/Modal/Confirm.tsx
@@ -0,0 +1,35 @@
+import Button from '../Button';
+import ModalWrapper from './Wrapper';
+
+interface ModalProps {
+  text: string;
+  isOpen: boolean;
+}
+
+interface ConfirmModalProps extends ModalProps {
+  onClickConfirm: () => void;
+  onClickCancel: () => void;
+}
+
+export default function ConfirmModal({
+  isOpen,
+  text,
+  onClickCancel,
+  onClickConfirm,
+}: ConfirmModalProps) {
+  return (
+    <ModalWrapper isOpen={isOpen}>
+      <div className='absolute flex flex-col items-center w-[66%] max-w-2xl gap-8 p-6 transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-lg top-1/2 left-1/2 rounded-2xl'>
+        <div className='text-sm text-center whitespace-pre-line'>{text}</div>
+        <div className='flex gap-4'>
+          <Button size='small' color='blue' onButtonClick={onClickConfirm}>
+            예
+          </Button>
+          <Button size='small' color='gray' onButtonClick={onClickCancel}>
+            아니오
+          </Button>
+        </div>
+      </div>
+    </ModalWrapper>
+  );
+}
