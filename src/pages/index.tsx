86e619e5fc3988d3ea7bf865b69ae6ee1f044350
유지영 <yj0zero@gmail.com>
2023-05-16 22:05:32 +0900

    feat: 메인페이지 레이아웃 설정 및 관련 공통 컴포넌트 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,107 @@
+import Slider from 'react-slick';
+
+import BookShelfPreview from '@/components/common/BookShelfPreviw';
+import ImageComponent from '@/components/common/ImageComponent';
+
+const BOOKSHELFDUMMY = [
+  {
+    memberId: '1',
+    img: [
+      'https://ifh.cc/g/2h1yGb.jpg',
+      'https://ifh.cc/g/3yaCkO.jpg',
+      'https://ifh.cc/g/HRw0xB.jpg',
+      'https://ifh.cc/g/Mb8x6h.jpg',
+    ],
+  },
+  {
+    memberId: '2',
+    img: [
+      'https://ifh.cc/g/2h1yGb.jpg',
+      'https://ifh.cc/g/3yaCkO.jpg',
+      'https://ifh.cc/g/HRw0xB.jpg',
+      'https://ifh.cc/g/Mb8x6h.jpg',
+    ],
+  },
+  {
+    memberId: '3',
+    img: [
+      'https://ifh.cc/g/2h1yGb.jpg',
+      'https://ifh.cc/g/3yaCkO.jpg',
+      'https://ifh.cc/g/HRw0xB.jpg',
+      'https://ifh.cc/g/Mb8x6h.jpg',
+    ],
+  },
+  {
+    memberId: '4',
+    img: [
+      'https://ifh.cc/g/2h1yGb.jpg',
+      'https://ifh.cc/g/3yaCkO.jpg',
+      'https://ifh.cc/g/HRw0xB.jpg',
+      'https://ifh.cc/g/Mb8x6h.jpg',
+    ],
+  },
+];
+
+const FEEDDUMMY = [
+  {
+    memberId: '1',
+    img: 'https://ifh.cc/g/2h1yGb.jpg',
+  },
+  {
+    memberId: '2',
+    img: 'https://ifh.cc/g/3yaCkO.jpg',
+  },
+  {
+    memberId: '3',
+    img: 'https://ifh.cc/g/HRw0xB.jpg',
+  },
+  {
+    memberId: '4',
+    img: 'https://ifh.cc/g/Mb8x6h.jpg',
+  },
+];
+
 export default function Home() {
-  return <></>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <main>
+      <section>
+        <h1 className='text-xl m-3'>오늘의 책장</h1>
+        <div className='mx-10'>
+          <Slider {...settings}>
+            {BOOKSHELFDUMMY.map(({ img, memberId }) => (
+              <BookShelfPreview
+                key={memberId}
+                imageSrcArr={img}
+                memberId={memberId}
+              />
+            ))}
+          </Slider>
+        </div>
+      </section>
+      <section className='mt-10'>
+        <h1 className='text-xl m-3'>오늘의 기록</h1>
+        <div className='grid grid-cols-3  gap-2 m-2'>
+          {FEEDDUMMY.map(({ img, memberId }) => (
+            <ImageComponent
+              key={memberId}
+              lazy={true}
+              placeholder=''
+              src={img}
+              size='feed-small'
+              alt='기록 피드 이미지'
+              imgStyle='w-full h-full'
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
