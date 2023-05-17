9b8be502bcb294b7bda3e126f0253faf23d20d96
bohongu <jaehong2782@naver.com>
2023-05-17 22:10:55 +0900

    feat: backdrop 클릭 시 모달 닫기 구현

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { useSetRecoilState } from 'recoil';
+
+import { modalStateAtom } from '@/recoil/modal';
 
 const BackDrop = () => {
+  const setModal = useSetRecoilState(modalStateAtom);
   return (
-    <div className='fixed w-full h-screen z-20 bg-[rgba(0,0,0,0.75)] left-0 top-0' />
+    <div
+      className='fixed w-full h-screen z-20 bg-[rgba(0,0,0,0.75)] left-0 top-0'
+      onClick={() => setModal(false)}
+    />
   );
 };
 
