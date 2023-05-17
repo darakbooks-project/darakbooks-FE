008e94553eef6e965e21bed26f280dbc0e0685ec
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-17 15:11:08 +0900

    feat: 도서 검색 페이지 마크업

--- src/pages/book/search/index.tsx
+++ src/pages/book/search/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import tw from 'tailwind-styled-components';
+
+import BottomNav from '@/components/common/BottomNav';
+import SearchResult from '@/components/common/ListItem/SearchResult';
+import SearchInput from '@/components/common/SearchInput';
 
 const BookSearchPage = () => {
-  return <div>도서 검색 페이지</div>;
+  return (
+    <Container>
+      <SearchInput />
+      <SearchResult
+        src='https://image.yes24.com/goods/15058512/XL'
+        imageSize='not-feed-small'
+        title='미움 받을 용기'
+        author='기시미 이치로'
+        publisher='이치로 출판사'
+      />
+      <BottomNav />
+    </Container>
+  );
 };
 
 export default BookSearchPage;
+
+const Container = tw.div`
+  w-[90%]
+  mx-auto
+  mt-8
+`;
