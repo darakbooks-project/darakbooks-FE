3f689b853fbdd3daef2366b2ee8a2364a576c29f
유지영 <yj0zero@gmail.com>
2023-05-23 16:53:11 +0900

    refactor: Tab 공통 컴포넌트 내부에서 클릭 이벤트 처리하도록 변경

--- src/components/common/Tab/TabItem.tsx
+++ src/components/common/Tab/TabItem.tsx
@@ -2,11 +2,12 @@ import Link from 'next/link';
 
 interface Props {
   title: string;
-  active?: boolean;
+  active: boolean;
   navigatePath: string;
+  tabPosition: string;
 }
 
-const TabItem = ({ title, active, navigatePath }: Props) => {
+const TabItem = ({ title, active, navigatePath, tabPosition }: Props) => {
   return (
     <Link
       className={`w-[50%] inline-flex justify-center items-end w-[140px] h-[60px] border-b-2 cursor-pointer ${
@@ -14,9 +15,12 @@ const TabItem = ({ title, active, navigatePath }: Props) => {
           ? 'border-blue-800 text-blue-800'
           : 'border-gray-400 text-gray-400'
       }`}
+      data-position={tabPosition}
       href={navigatePath}
     >
-      <div className='pb-[7px]'>{title}</div>
+      <div className='pb-[7px]' data-position={tabPosition}>
+        {title}
+      </div>
     </Link>
   );
 };
--- src/components/common/Tab/index.tsx
+++ src/components/common/Tab/index.tsx
@@ -1,31 +1,45 @@
+import { useState } from 'react';
+
 import TabItem from './TabItem';
 
 interface Props {
-  lefttext: string;
-  righttext: string;
+  leftText: string;
+  rightText: string;
   leftNavigatePath: string;
   rightNavigatePath: string;
-  position: 'left' | 'right';
+  defaultPosition: 'left' | 'right';
 }
 
 const Tab = ({
-  lefttext,
-  righttext,
+  leftText,
+  rightText,
   leftNavigatePath,
   rightNavigatePath,
-  position,
+  defaultPosition,
 }: Props) => {
+  const [tabPosition, setTabPosition] = useState(defaultPosition);
+
   return (
-    <div className='w-[100%] flex justify-center'>
+    <div
+      className='w-[100%] flex justify-center'
+      onClick={(e) => {
+        if (!(e.target instanceof HTMLElement)) return;
+        const position = e.target.dataset.position;
+        if (position === 'left') setTabPosition('left');
+        if (position === 'right') setTabPosition('right');
+      }}
+    >
       <TabItem
-        title={lefttext}
-        active={position === 'left'}
+        title={leftText}
+        active={tabPosition === 'left'}
         navigatePath={leftNavigatePath}
+        tabPosition='left'
       />
       <TabItem
-        title={righttext}
-        active={position === 'right'}
+        title={rightText}
+        active={tabPosition === 'right'}
         navigatePath={rightNavigatePath}
+        tabPosition='right'
       />
     </div>
   );
