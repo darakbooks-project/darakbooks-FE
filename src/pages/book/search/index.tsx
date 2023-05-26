cd56a167a29be2eb3c85600652fa3b2d7cebcbe4
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-26 21:07:14 +0900

    refactor: 무한 스크롤 부분 컴포넌트로 분리

--- src/pages/book/search/index.tsx
+++ src/pages/book/search/index.tsx
@@ -1,65 +1,21 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useState } from 'react';
 import tw from 'tailwind-styled-components';
 
-import { getBookSearchResultData } from '@/api/book';
-import SearchResultList from '@/components/book/search/SearchResultList';
+import InfinityScrollLists from '@/components/book/search/InfinityScrollLists';
 import BottomNav from '@/components/common/BottomNav';
 import SearchInput from '@/components/common/SearchInput';
 
 const BookSearchPage = () => {
-  const { ref, inView } = useInView();
-  const [page, setPage] = useState(0);
   const [searchKeyword, setSearchKeyword] = useState('');
 
   const onSubmit = (keyword: string) => {
     setSearchKeyword(keyword);
-    setPage(1);
   };
-
-  const {
-    data: bookSearchResultLists,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    status,
-  } = useInfiniteQuery(
-    ['book', 'search', 'result', 'list', searchKeyword],
-    ({ pageParam = page }) => getBookSearchResultData(searchKeyword, pageParam),
-    {
-      onSuccess: () => setPage((prev) => prev + 1),
-      getNextPageParam: (lastPage) => {
-        if (lastPage.is_end) return;
-
-        return page;
-      },
-      enabled: !!searchKeyword,
-    },
-  );
-
-  useEffect(() => {
-    if (inView) fetchNextPage();
-  }, [fetchNextPage, inView]);
 
   return (
     <Container>
       <SearchInput onSubmit={onSubmit} />
-      {status === 'success' &&
-        bookSearchResultLists.pages.map(({ documents }, index) =>
-          documents.length > 0 && documents[index] ? (
-            <SearchResultList
-              key={documents[index].isbn}
-              listData={documents}
-            />
-          ) : (
-            <div key='not search result'>검색 결과가 없습니다.</div>
-          ),
-        )}
-
-      <div ref={ref}>{isFetchingNextPage && hasNextPage && 'Loading...'}</div>
-      {!isFetchingNextPage}
+      <InfinityScrollLists searchKeyword={searchKeyword} />
       <BottomNav />
     </Container>
   );
