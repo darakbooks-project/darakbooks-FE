66f64b0f4bcef5a18337ba147e88b4f416aa5219
bohongu <jaehong2782@naver.com>
2023-05-27 14:21:33 +0900

    api: 이미지 등록 api

---
+++ src/api/image.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const registerImageApi = async (image: FormData) => {
+  try {
+    const { data } = await axios.post(`${BASE_URL}/records/photo`, image);
+    return data;
+  } catch (error) {
+    throw new Error('사진 등록 실패');
+  }
+};
