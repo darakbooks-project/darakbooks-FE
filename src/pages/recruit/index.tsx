54395724d1559d58377ec9123c82c43eb9ad5426
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-28 23:22:59 +0900

    refactor: 컴포넌트로 분리

--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -1,62 +1,20 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components';
 
-import { getBookSearchResultData } from '@/api/book';
-import SearchResultList from '@/components/book/search/SearchResultList';
+import RecruitInfinityScrollLists from '@/components/recruit/RecruitInfinityScrollLists';
 
 const RecruitPage = () => {
-  const { ref, inView } = useInView();
-  const [page, setPage] = useState(1);
-
-  const {
-    data: bookSearchResultLists,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    status,
-  } = useInfiniteQuery(
-    ['reading', 'personnel', 'recruit'],
-    ({ pageParam = 1 }) => getBookSearchResultData('미움 받을 용기', pageParam),
-    {
-      onSuccess: () => setPage((prev) => prev + 1),
-      onError: (error) => console.error(error),
-      getNextPageParam: (lastPage) => {
-        if (lastPage.is_end) return;
-
-        return page;
-      },
-    },
-  );
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if (inView) fetchNextPage();
-  }, [fetchNextPage, inView]);
+    setIsMounted(true);
+  }, []);
 
   return (
     <>
-      <Container>
-        <h1>독서 인원 모집 페이지</h1>
-        {status === 'success' && (
-          <>
-            {bookSearchResultLists.pages.map(
-              ({ documents }, index) =>
-                documents.length > 0 &&
-                documents[index] && (
-                  <SearchResultList
-                    key={documents[index].isbn}
-                    listData={documents}
-                  />
-                ),
-            )}
-            <Bottom ref={ref}>
-              {isFetchingNextPage && hasNextPage && 'Loading...'}
-            </Bottom>
-          </>
-        )}
-      </Container>
+      <h1>독서 인원 모집 페이지</h1>
+      {isMounted && <RecruitInfinityScrollLists />}
       <PersonnelRecruitButton href='/recruit/write'>
         독서 인원 모집 글 작성
       </PersonnelRecruitButton>
@@ -65,10 +23,6 @@ const RecruitPage = () => {
 };
 
 export default RecruitPage;
-
-const Container = tw.div``;
-
-const Bottom = tw.div``;
 
 const PersonnelRecruitButton = tw(Link)`
   fixed
