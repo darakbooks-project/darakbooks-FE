bbe2b27df4f4a5fb0fd4db566da668676dc60cc7
bohongu <jaehong2782@naver.com>
2023-05-28 19:36:01 +0900

    design: 기본 bg-color 설정

--- src/pages/book/detail/index.tsx
+++ src/pages/book/detail/index.tsx
@@ -11,7 +11,7 @@ const DUMMY = [
 const BookDetailPage = () => {
   return (
     <div className='flex flex-col gap-2.5 pb-2.5 '>
-      <section className='h-[30rem] border border-solid border-[red]'>
+      <section className='h-[30rem] border border-solid  bg-[#ffffff]'>
         <div className='absolute w-44 h-64 left-[calc(50%_-_170px_/_2)] rounded-[0px_3px_3px_0px] top-[107px] drop-shadow-xl border-basic'>
           <Image src={search} alt='테스트' fill />
         </div>
@@ -21,7 +21,7 @@ const BookDetailPage = () => {
           <h4 className='text-[13px] text-[#999797]'>생각의 날개</h4>
         </article>
       </section>
-      <section className='border p-4 border-solid border-[green]'>
+      <section className='border p-4 border-solid bg-[#ffffff]'>
         <h2 className='not-italic font-bold text-xl leading-[29px] mb-4'>
           책 소개
         </h2>
@@ -31,7 +31,7 @@ const BookDetailPage = () => {
           카운슬링. 어쩌자고 이런 성격일까 싶다면, 이 책을 한번 읽어보자!
         </p>
       </section>
-      <section className='border p-4 border-solid border-[black]'>
+      <section className='border p-4 border-solid bg-[#ffffff]'>
         <h2 className='not-italic font-bold text-xl leading-[29px] mb-4'>
           관련 기록
         </h2>
@@ -54,7 +54,7 @@ const BookDetailPage = () => {
           ))}
         </ul>
       </section>
-      <section className='flex justify-evenly items-center w-full px-0 py-4'>
+      <section className='flex justify-evenly items-center w-full px-0 py-4 bg-[#ffffff]'>
         <button className='flex justify-center items-center box-border w-28 h-16 shadow-[4px_4px_8px_rgba(0,0,0,0.15)] not-italic font-bold text-base leading-[19px] text-[#5a987d] rounded-md border-2 border-solid border-[#5a987d]'>
           담기
         </button>
