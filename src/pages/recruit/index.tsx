66c96dc33549619df3e52643e884fd107facb145
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-05-28 14:39:38 +0900

    feat: 무한 스크롤 리스트
    현재 독서 인원 모집 리스트 데이터가 없어서 kakao 도서 api 데이터를 임시로 사용했습니다.
    API가 완성되면 데이터 수정하고 현재는 레이아웃 잡는 목적으로 사용했습니다.

--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import tw from 'tailwind-styled-components';
+
+import { getBookSearchResultData } from '@/api/book';
+import SearchResultList from '@/components/book/search/SearchResultList';
 
 const RecruitPage = () => {
-  return <div>독서 인원 모집 페이지</div>;
+  const { ref, inView } = useInView();
+  const [page, setPage] = useState(1);
+
+  const {
+    data: bookSearchResultLists,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery(
+    ['reading', 'personnel', 'recruit'],
+    ({ pageParam = 1 }) => getBookSearchResultData('미움 받을 용기', pageParam),
+    {
+      onSuccess: () => setPage((prev) => prev + 1),
+      onError: (error) => console.error(error),
+      getNextPageParam: (lastPage) => {
+        if (lastPage.is_end) return;
+
+        return page;
+      },
+    },
+  );
+
+  useEffect(() => {
+    if (inView) fetchNextPage();
+  }, [fetchNextPage, inView]);
+
+  return (
+    <Container>
+      <h1>독서 인원 모집 페이지</h1>
+      {status === 'success' && (
+        <>
+          {bookSearchResultLists.pages.map(
+            ({ documents }, index) =>
+              documents.length > 0 &&
+              documents[index] && (
+                <SearchResultList
+                  key={documents[index].isbn}
+                  listData={documents}
+                />
+              ),
+          )}
+          <Bottom ref={ref}>
+            {isFetchingNextPage && hasNextPage && 'Loading...'}
+          </Bottom>
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default RecruitPage;
+
+const Container = tw.div``;
+
+const Bottom = tw.div``;
