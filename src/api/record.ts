22b315402cd8a06071ce7ac0c22869b7e12c479b
bohongu <jaehong2782@naver.com>
2023-05-31 20:06:41 +0900

    api: 독서 기록 등록 api 연결

--- src/api/record.ts
+++ src/api/record.ts
@@ -2,12 +2,40 @@ import axios from 'axios';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-// 이미지 등록 api
+export interface bookRecordDataProps {
+  record: {
+    title: string;
+    thumbnail: string;
+    bookIsbn: string;
+    text: string;
+    recordImg: string;
+    recordImgUrl: string;
+    tags?: {
+      id: number;
+      data: string | number;
+    }[];
+    readAt: string;
+  };
+}
+
+// 이미지 등록
 export const registerImageApi = async (image: FormData) => {
   try {
     const { data } = await axios.post(`${BASE_URL}/records/photo`, image);
     return data;
   } catch (error) {
     throw new Error('사진 등록 실패');
+  }
+};
+
+// 독서기록 등록
+export const registerBookRecordApi = async (
+  bookRecordData: bookRecordDataProps,
+) => {
+  try {
+    const { data } = await axios.post(`${BASE_URL}/records`, bookRecordData);
+    return data;
+  } catch (error) {
+    throw new Error('독서 기록 등록 실패');
   }
 };
--- src/pages/book/record/index.tsx
+++ src/pages/book/record/index.tsx
@@ -4,10 +4,15 @@ import Link from 'next/link';
 import React, { useRef, useState } from 'react';
 import DatePicker from 'react-datepicker';
 
-import { registerImageApi } from '@/api/record';
+import {
+  bookRecordDataProps,
+  registerBookRecordApi,
+  registerImageApi,
+} from '@/api/record';
 import styles from '@/components/book/record/Calendar.module.css';
 import useImage from '@/hooks/useImage';
 import useInput from '@/hooks/useInput';
+import { useRouter } from 'next/router';
 
 interface TagProps {
   id: number;
@@ -16,6 +21,7 @@ interface TagProps {
 
 const BookRecordPage = () => {
   const registerImage = useMutation(registerImageApi);
+  const registerBookRecord = useMutation(registerBookRecordApi);
 
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [description, setDescription] = useState('');
@@ -23,6 +29,7 @@ const BookRecordPage = () => {
   const [tagList, setTagList] = useState<TagProps[]>([]);
   const id = useRef(0);
   const [postImage, setPostImage] = useImage({}, registerImage);
+  const router = useRouter();
 
   const today = new Intl.DateTimeFormat('kr').format(new Date());
 
@@ -75,7 +82,28 @@ const BookRecordPage = () => {
       day >= 10 ? day : '0' + day
     }`;
 
-    console.log(postImage, description, formattedDate, tagList);
+    const data: bookRecordDataProps = {
+      record: {
+        title: 'title test',
+        thumbnail: 'thumbnail test',
+        bookIsbn: 'bookIsbn text',
+        text: description,
+        recordImg: postImage.name as string,
+        recordImgUrl: postImage.url as string,
+        tags: tagList,
+        readAt: formattedDate,
+      },
+    };
+
+    registerBookRecord.mutate(data, {
+      onSuccess: () => {
+        alert('독서 기록 성공');
+        router.push('/');
+      },
+      onError: (error) => {
+        alert(error);
+      },
+    });
   };
 
   return (
