3fcb8f6fcce333d65463b3c4c3a4b866ed500697
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-06-01 13:04:25 +0900

    feat: 기존 액세스 토큰 유효기간 만료 시 새로운 액세스 토큰 발급 (#90)
    * feat: Axios instance 생성
    
    * feat: 응답 인터셉터 설정
    
    * fix: 로그인 함수 2번에서 1번만 호출되도록 수정
    
    2번 호출하면 400 Error 발생함으로 인해 1번만 호출하도록 수정
    
    * feat: axios instance header에 token 설정 함수 제작
    
    * refactor: 로그인 및 토큰 재발급 함수 수정
    
    1. 배포된 API URL로 수정
    2. 로그인 시 axios instance header에 토큰 설정
    3. 토큰 재발급 시 header에 토큰 설정 및 기존의 요청 정보를 통해 재요청

---
+++ src/api/axios.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosError, CreateAxiosDefaults } from 'axios';
+
+import { silentRefresh } from './auth';
+
+const options: CreateAxiosDefaults = {
+  headers: {
+    'Accept': '*/*',
+    'Content-Type': 'application/json',
+  },
+  timeout: 3000,
+};
+
+export const axiosInstance = axios.create({
+  ...options,
+});
+
+const responseHandler = async (error: AxiosError) => {
+  const { config: originRequest, response } = error;
+
+  if (response) {
+    const { statusCode } = response.data as { statusCode: number };
+    console.warn(statusCode);
+
+    if (originRequest && statusCode === 401) {
+      return silentRefresh(originRequest);
+    }
+  }
+
+  return Promise.reject(error);
+};
+
+axiosInstance.interceptors.response.use(null, responseHandler);
