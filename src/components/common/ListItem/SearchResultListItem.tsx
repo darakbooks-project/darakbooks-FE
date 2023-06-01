0e622e0970d84cd19d22a9cee047d6f4586533d2
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-06-01 13:05:11 +0900

    feat: 독서 인원 모집 페이지 제작 (#96)
    * feat: 무한 스크롤 리스트
    
    현재 독서 인원 모집 리스트 데이터가 없어서 kakao 도서 api 데이터를 임시로 사용했습니다.
    API가 완성되면 데이터 수정하고 현재는 레이아웃 잡는 목적으로 사용했습니다.
    
    * feat: 리스트 아이템 클릭 시 독서 인원 모집 상세 페이지 이동
    
    * feat: 버튼 클릭 시 독서 모임 인원 모집 글 작성 페이지로 이동
    
    * fix: a태그 안에 a 태그를 넣으면 에러 발생하므로 수정
    
    * refactor: 컴포넌트로 분리
    
    * feat: SSR을 통해 데이터 prefetch

--- src/components/common/ListItem/SearchResultListItem.tsx
+++ src/components/common/ListItem/SearchResultListItem.tsx
@@ -21,25 +21,31 @@ const SearchResultListItem = ({
   clickShiftPath,
 }: SearchResultListItemProps) => {
   return (
-    <Link
-      href={clickShiftPath === 'search' ? '/book/detail' : '/book/select'}
-      className='w-[100%] flex items-center bg-yellow-500 px-[20px] py-[15px] cursor-pointer'
-    >
-      <div className='w-[50%]'>
-        <BookImage
-          lazy={true}
-          src={src}
-          placeholder='스켈레톤'
-          alt='책 선택 리스트 아이템의 사진 입니다.'
-          size={imageSize}
-        />
-      </div>
-      <div className='w-[50%] flex flex-col font-bold text-[15px]'>
-        <span>{title}</span>
-        <span>{`${author[0]} ${author[1] ? `|  ${author[1]}` : ''}`}</span>
-        <span>{publisher}</span>
-      </div>
-    </Link>
+    <>
+      <Link
+        href={clickShiftPath === 'search' ? '/book/detail' : '/book/select'}
+        className='w-[100%] flex items-center bg-yellow-500 px-[20px] py-[15px] cursor-pointer'
+      >
+        <div className='w-[50%]'>
+          <BookImage
+            lazy={true}
+            src={src}
+            placeholder='스켈레톤'
+            alt='책 선택 리스트 아이템의 사진 입니다.'
+            size={imageSize}
+          />
+        </div>
+        <div className='w-[50%] flex flex-col font-bold text-[15px]'>
+          <span>{title}</span>
+          <span>{`${author[0]} ${author[1] ? `|  ${author[1]}` : ''}`}</span>
+          <span>{publisher}</span>
+        </div>
+      </Link>
+      {/* by 민형, 독서 인원 모집 리스트 아이템 클릭 시 상세 페이지로 이동하는 것을 확인하기 위한 임시 코드이므로 나중에 제거_230528 */}
+      <Link href='/recruit/detail?recruitId=1' className='bg-white'>
+        독서 상세페이지로
+      </Link>
+    </>
   );
 };
 
