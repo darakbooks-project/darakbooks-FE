ab446cade632a8e77256a52b333181f83654020a
유지영 <yj0zero@gmail.com>
2023-06-02 20:40:33 +0900

    design: 책장 디자인 구현 및 next/Image로 변경

--- src/components/common/BookShelfPreview.tsx
+++ src/components/common/BookShelfPreview.tsx
@@ -1,32 +1,61 @@
+import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 
-import ImageComponent from './ImageComponent';
-
 interface BookShelfPreviewProps {
-  imageSrcArr: string[];
+  nickname: string;
+  imageSrcArr: StaticImageData[];
   memberId: string;
 }
 
-const BookShelfPreview = ({ imageSrcArr, memberId }: BookShelfPreviewProps) => {
+const BookShelfPreview = ({
+  nickname,
+  imageSrcArr,
+  memberId,
+}: BookShelfPreviewProps) => {
   return (
-    <Link
-      href={`bookshelf?memberId=${memberId}`}
-      className='w-[100%] h-[130px] flex justify-evenly items-center bg-gray-400 cursor-pointer xxs:[&>*:nth-child(n+3)]:hidden'
+    <div
+      className='w-[100%] h-[11.6875rem] bg-[#FFFEF8] drop-shadow-md rounded-t-md cursor-pointer'
+      style={{
+        perspective: '300px',
+        transform: 'translate3d(0,0,0)',
+      }}
     >
-      {imageSrcArr.map((src: string) => {
-        return (
-          <div key={src} className='xs:last:hidden'>
-            <ImageComponent
-              lazy={true}
-              placeholder=''
-              src={src}
-              alt='책장 미리보기 속의 책입니다!'
-              size='not-feed-small'
-            />
+      <Link href={`bookshelf?memberId=${memberId}`} className=' flex'>
+        <div className='flex flex-col w-[100%]'>
+          <p className='text-xs ml-[calc((50%-(87.86px/2)-87.86px)/2)] relative top-3 xxs:ml-[calc((50%-87.86px)/2)]'>
+            {nickname}의 서재
+          </p>
+          <div className='flex justify-evenly items-center w-[100%] relative top-6'>
+            {imageSrcArr.map((src: StaticImageData) => {
+              return (
+                <div
+                  key={src.src}
+                  className='xxs:last:hidden drop-shadow-lg z-10'
+                >
+                  <Image
+                    src={src}
+                    width={87}
+                    height={130}
+                    placeholder='blur'
+                    blurDataURL={src.src}
+                    alt='추천 책장 책 표지'
+                    className='rounded-r'
+                    style={{ width: 87, height: 130 }}
+                  />
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </Link>
+        </div>
+      </Link>
+      <div
+        className='bg-[#E2D6CA] w-[99%] h-[14px] relative top-4 mx-auto'
+        style={{
+          transform: 'rotateX(40deg)',
+        }}
+      ></div>
+      <div className='w-[100%] h-[10px] bg-white relative top-[16px]'></div>
+    </div>
   );
 };
 
