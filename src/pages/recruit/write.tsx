4705d64205f48b4f016410e8ec8814bbb20c7b49
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-06-04 12:38:36 +0900

    feat: 독서 인원 모집 개설 페이지 제작 (#99)
    * feat: 독서 인원 모집 개설 페이지 마크업
    
    * feat: 지역 선택 기능
    
    * feat: 개설 기능 구현
    
    * refactor: 컴포넌트로 분리

--- src/pages/recruit/write.tsx
+++ src/pages/recruit/write.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import { useMutation } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import tw from 'tailwind-styled-components';
+
+import Header from '@/components/common/Header';
+import RecruitOpenForm from '@/components/recruit/RecruitOpenForm';
+import {
+  ClassOpenChangeStateObjProps,
+  ClassOpenStateObjProps,
+} from '@/types/recruit';
 
 const RecruitWritePage = () => {
-  return <div>모집 공고 생성 페이지</div>;
+  const [className, setClassName] = useState('');
+  const [classType, setClassType] = useState('on');
+  const [classRegion, setClassRegion] = useState('서울');
+  const [classDescription, setClassDescription] = useState('');
+
+  const classStateObj: ClassOpenStateObjProps = {
+    className,
+    classType,
+    classRegion,
+    classDescription,
+  };
+
+  const classChangeStateObj: ClassOpenChangeStateObjProps = {
+    changeClassName: (e) => {
+      setClassName(e.target.value);
+    },
+    changeClassType: (type) => {
+      if (type === 'on') setClassType('on');
+      else setClassType('off');
+    },
+    changeClassRegion: (e) => {
+      setClassRegion(e.target.value);
+    },
+    changeClassDescription: (e) => {
+      setClassDescription(e.target.value);
+    },
+  };
+
+  const openReadingClass = () => {
+    // by 민형, API 호출 함수(POST) 및 객체 + isRecruiting: true, 전달_230529
+    // useMutation(classState);
+  };
+
+  return (
+    <Container>
+      <Header title='개설 하기' />
+      <Wrapper>
+        <RecruitOpenForm
+          classStateObj={classStateObj}
+          classChangeStateObj={classChangeStateObj}
+        />
+        <ClassOpenButton onClick={openReadingClass}>모임 개설</ClassOpenButton>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default RecruitWritePage;
+
+const Container = tw.div``;
+
+const Wrapper = tw.div`
+  flex
+  flex-col
+`;
+
+const ClassOpenButton = tw.button`  
+  bg-blue-300
+`;
