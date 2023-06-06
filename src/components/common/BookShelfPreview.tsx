fbd277b63e103b2ed2b70aa10cc78f1f5ffa8b9f
유지영 <yj0zero@gmail.com>
2023-06-06 22:18:05 +0900

    design: width 작을 때 책장의 책 크기 변경되도록 수정

--- src/components/common/BookShelfPreview.tsx
+++ src/components/common/BookShelfPreview.tsx
@@ -14,7 +14,7 @@ const BookShelfPreview = ({
 }: BookShelfPreviewProps) => {
   return (
     <div
-      className='w-[100%] h-[11.6875rem] bg-[#FFFEF8] drop-shadow-md rounded-t-md cursor-pointer'
+      className='w-[100%] h-[187px] bg-[#FFFEF8] drop-shadow-md rounded-t-md cursor-pointer xxs:h-[10rem]'
       style={{
         perspective: '300px',
         transform: 'translate3d(0,0,0)',
@@ -22,16 +22,13 @@ const BookShelfPreview = ({
     >
       <Link href={`bookshelf?memberId=${memberId}`} className=' flex'>
         <div className='flex flex-col w-[100%]'>
-          <p className='text-xs ml-[calc((50%-(87.86px/2)-87.86px)/2)] relative top-3 xxs:ml-[calc((50%-87.86px)/2)]'>
+          <p className='text-xs ml-[calc((50%-(87.86px/2)-87.86px)/2)] relative top-3 xxs:ml-[calc((50%-87.86px)/2-0.5rem)]'>
             {nickname}의 서재
           </p>
-          <div className='flex justify-evenly items-center w-[100%] relative top-6'>
+          <div className='flex justify-evenly items-center w-[100%] relative top-6 xs:p-1'>
             {imageSrcArr.map((src: StaticImageData) => {
               return (
-                <div
-                  key={src.src}
-                  className='xxs:last:hidden drop-shadow-lg z-10'
-                >
+                <div key={src.src} className='drop-shadow-lg z-10'>
                   <Image
                     src={src}
                     width={87}
@@ -39,8 +36,7 @@ const BookShelfPreview = ({
                     placeholder='blur'
                     blurDataURL={src.src}
                     alt='추천 책장 책 표지'
-                    className='rounded-r'
-                    style={{ width: 87, height: 130 }}
+                    className='rounded-r max-w-[97px] max-h-[130px] w-full h-full xs:px-1'
                   />
                 </div>
               );
