813385a25d1085bad577ba4201f4c0e0fac634aa
JiYoung <yj0zero@gmail.com>
2023-06-07 15:12:45 +0900

    design: 메인 페이지 디자인 변경 사항 반영 (#130)
    * rename: layout 타입 파일 d.ts로 변경
    
    * design: 인기모임 디자인 변경사항 반영
    
    * feat: 그룹 api response 데이터 타입 지정
    
    * refactor: avatar 컴포넌트 size 수정
    
    * design: width 작을 때 책장의 책 크기 변경되도록 수정

--- src/types/recruit.d.ts
+++ src/types/recruit.d.ts
@@ -11,3 +11,29 @@ export interface ClassOpenChangeStateObjProps {
   changeClassRegion: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   changeClassDescription: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
+
+export interface GroupList {
+  group_id: number;
+  name: string;
+  recruitment_status: boolean;
+  meeting_type: string;
+  day: string;
+  time: string;
+  region: string;
+  description: string;
+  participant_limit: number;
+  open_chat_link: string;
+  group_lead: string;
+  userGroup: UserGroup[];
+}
+
+export interface UserGroup {
+  age: string;
+  gender: string;
+  groups: number[];
+  nickname: string;
+  profileImg: string;
+  provider: string;
+  userId: string;
+  userInfo: string;
+}
