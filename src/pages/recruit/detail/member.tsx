afb7de702b3cea965ad70e7fd3621bc2eb92b1eb
유지영 <yj0zero@gmail.com>
2023-06-10 23:20:59 +0900

    feat: 모임 멤버 리스트 페이지 구현

--- src/pages/recruit/detail/member.tsx
+++ src/pages/recruit/detail/member.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
 
+import Header from '@/components/common/Header';
+import MemberListItem from '@/components/recruit/detail/member/MemberListItem';
+
+const GROUPMEMBER = {
+  userGroup: [
+    {
+      userId: 1,
+      nickname: '민아',
+      profileImg: 'https://via.placeholder.com/40',
+      userInfo: '계속해서 책 읽기!',
+      gender: 'female',
+      age: '23',
+      provider: 'kakao',
+      groups: [2, 3, 4],
+    },
+    {
+      userId: 2,
+      nickname: '민아',
+      profileImg: 'https://via.placeholder.com/40',
+      userInfo: '얌냠냠',
+      gender: 'female',
+      age: '23',
+      provider: 'kakao',
+      groups: [2, 3, 4],
+    },
+    {
+      userId: 3,
+      nickname: '민아',
+      profileImg: 'https://via.placeholder.com/40',
+      userInfo: '움냠얀',
+      gender: 'female',
+      age: '23',
+      provider: 'kakao',
+      groups: [2, 3, 4],
+    },
+  ],
+};
+
 const member = () => {
-  return <div>member</div>;
+  return (
+    <div className='bg-white h-full'>
+      <div className='pt-14 pb-5'>
+        <Header title='참여인원' />
+      </div>
+      <div className=' border border-[#EBEAEA]' />
+      <section className=' px-5'>
+        <p className=' py-4 text-[#67A68A] text-sm'>
+          멤버 {GROUPMEMBER.userGroup.length}
+        </p>
+        <ul>
+          {GROUPMEMBER.userGroup.map((user) => (
+            <MemberListItem key={user.userId} {...user} />
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default member;
