408077d544e6837b0608d2601a7566e1ef394c11
유지영 <yj0zero@gmail.com>
2023-06-10 18:16:29 +0900

    design: 독서 모임 상세페이지 디자인 적용

--- src/pages/recruit/detail.tsx
+++ src/pages/recruit/detail.tsx
@@ -1,96 +1,137 @@
-import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 import Avatar from '@/components/common/Avartar';
+import RecruitNotification from '@/components/recruit/detail/RecruitNotification';
+import RecruitUserManagement from '@/components/recruit/detail/RecruitUserManagement';
 import { NextPageWithLayout } from '@/types/layout';
 
 const DUMMY = {
-  data: {
-    groupId: 1,
-    groupName: '독서모임~',
-    groupImg: 'https://via.placeholder.com/400x200',
-    endDate: '2023-06-10',
-    online: false,
-    people: 10,
-    location: '서울',
-    description: '독서 모임입니다~',
-    notice: '공지입니다',
-    isAdmin: false,
-    isMember: true,
-    recruiting: true,
-    admin: {
-      userId: 1,
-      nickname: '일이삼',
+  group_id: 1,
+  name: '책 읽는 행복한 모임',
+  recruitment_status: true,
+  meeting_type: 'offline',
+  day: '월요일',
+  time: '15:00',
+  region: '서울 강서구',
+  description: '자유롭게 책을 읽고 공유하는 모임 입니다.',
+  participant_limit: 20,
+  open_chat_link: 'https://naver.com',
+  group_lead: '1',
+  isLeader: false,
+  isMember: false,
+  userGroup: [
+    {
+      user_id: '1',
+      nickname: '민아',
       profileImg: 'https://via.placeholder.com/40',
+      userInfo: 'user_info',
+      gender: 'female',
+      age: '23',
+      provider: 'kakao',
+      groups: [1, 3, 4],
     },
-    user: [
-      {
-        userId: 1,
-        nickname: '일일일',
-        profileImg: 'https://via.placeholder.com/40',
-      },
-      {
-        userId: 2,
-        nickname: '이이이',
-        profileImg: 'https://via.placeholder.com/40',
-      },
-      {
-        userId: 3,
-        nickname: '삼삼삼',
-        profileImg: 'https://via.placeholder.com/40',
-      },
-    ],
-  },
+    {
+      user_id: '2',
+      nickname: '여우',
+      profileImg: 'https://via.placeholder.com/40',
+      userInfo: 'user_info',
+      gender: 'female',
+      age: '23',
+      provider: 'kakao',
+      groups: [1, 3, 4],
+    },
+  ],
+};
+
+const GROUPLEADER = {
+  user_id: '1',
+  nickname: '민아',
+  profileImg: 'https://via.placeholder.com/40',
+  userInfo: 'user_info',
+  gender: 'female',
+  age: '23',
+  provider: 'kakao',
+  groups: [2, 3, 4],
 };
 
 const RecruitDetailPage: NextPageWithLayout = () => {
+  const NotificationState = [
+    { title: '요일/시간', detail: `매주 ${DUMMY.day} ${DUMMY.time}` },
+    { title: '활동 장소', detail: `매주 ${DUMMY.region}` },
+    {
+      title: '참여 인원',
+      detail: `${DUMMY.userGroup.length}/${DUMMY.participant_limit}`,
+    },
+    { title: '소통 방법', detail: `${DUMMY.open_chat_link}` },
+  ];
+
   return (
-    <main className='flex flex-col'>
-      {DUMMY.data.isMember && DUMMY.data.notice && (
-        <p className='bg-slate-100'>{DUMMY.data.notice}</p>
-      )}
-      <h1>{DUMMY.data.groupName}</h1>
-      <Image
-        src={DUMMY.data.groupImg}
-        width={400}
-        height={200}
-        alt='모임 설명 이미지'
-        priority
-      />
-      <p>{DUMMY.data.recruiting ? '모집중' : '모집완료'}</p>
-      <p>{DUMMY.data.online ? '온라인' : '오프라인'}</p>
-      <p>{!DUMMY.data.online && DUMMY.data.location}</p>
-      <p>{DUMMY.data.description}</p>
-      <div className='flex items-center'>
-        <span>모임장</span>
-        <Avatar
-          src={DUMMY.data.admin.profileImg}
-          shape='circle'
-          alt='모임장 프로필 이미지'
-          lazy={false}
-          placeholder=''
-        />
-      </div>
-      <div className='flex'>
-        {DUMMY.data.user.map((member) => (
+    <div className='h-full bg-white'>
+      <div className='w-full h-[17.5rem] bg-[#FFFCEA]' />
+      <main className='flex flex-col bg-white relative -top-10 px-5 rounded-t-[1.875rem] pb-20'>
+        <div className='flex py-6'>
           <Avatar
-            key={member.userId}
-            src={member.profileImg}
+            src={GROUPLEADER.profileImg}
             shape='circle'
-            alt='구성원 프로필 이미지'
-            lazy={false}
             placeholder=''
+            lazy={false}
+            alt='모임장 프로필 이미지'
+            width='w-[3.375rem]'
+            height='h-[3.375rem]'
+          />
+          <div className='pl-4'>
+            <h3 className='text-sm text-[#67A68A]'>
+              {DUMMY.recruitment_status ? '모집중' : '모집완료'}
+            </h3>
+            <h1 className='text-xl font-bold'>{DUMMY.name}</h1>
+          </div>
+        </div>
+        <p>{DUMMY.description}</p>
+        <div className='w-full h-[1px] bg-[#EBEAEA] my-8' />
+        <h3 className='text-[#67A68A] text-sm'>자세한 정보 알려드려요</h3>
+        <h2 className='text-xl pt-1 font-bold pb-6'>안내사항</h2>
+        {NotificationState.map(({ title, detail }) => (
+          <RecruitNotification
+            key={title}
+            title={title}
+            detail={detail}
+            meetingType={DUMMY.meeting_type === 'online'}
+            isMember={DUMMY.isMember}
           />
         ))}
-      </div>
-      {DUMMY.data.isAdmin ? (
-        <input type='text' className='border-basic' />
-      ) : DUMMY.data.isMember ? (
-        <button>탈퇴</button>
-      ) : (
-        <button>가입</button>
+        <div className='w-full h-[1px] bg-[#EBEAEA] my-8' />
+        <h3 className='text-[#67A68A] text-sm'>
+          함께 독서하며 소통하고 있어요
+        </h3>
+        <div className='flex justify-between items-center pb-5'>
+          <h2 className='text-xl pt-1 font-bold'>멤버 소개</h2>
+          <Link href={`/recruit/member`}>전체보기</Link>
+        </div>
+        <div className='flex'>
+          {DUMMY.userGroup.map((member) => (
+            <div key={member.user_id} className='pr-2'>
+              {/**해당 아바타 클릭 시 해당 유저의 책장 페이지로 이동 */}
+              <Avatar
+                src={member.profileImg}
+                shape='circle'
+                alt='구성원 프로필 이미지'
+                lazy={false}
+                placeholder=''
+                width='w-[3.1875rem]'
+                height='h-[3.1875rem]'
+              />
+            </div>
+          ))}
+        </div>
+      </main>
+      {!DUMMY.isLeader && (
+        <RecruitUserManagement
+          isMember={DUMMY.isMember}
+          recruitmentStatus={DUMMY.recruitment_status}
+        />
       )}
-    </main>
+    </div>
   );
 };
 
