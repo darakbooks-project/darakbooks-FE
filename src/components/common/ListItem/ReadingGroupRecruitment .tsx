04e49b37937723d117cd028c3c111d9aeb878605
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-06-12 16:26:00 +0900

    desgin: 독서 모임 리스트 페이지

--- src/components/common/ListItem/ReadingGroupRecruitment .tsx
+++ src/components/common/ListItem/ReadingGroupRecruitment .tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useSetRecoilState } from 'recoil';
+import tw from 'tailwind-styled-components';
 
 import { readingGroupInfinityScrollPositionAtom } from '@/recoil/recruit';
 import { GroupList } from '@/types/recruit';
@@ -31,23 +32,76 @@ const ReadingGroupRecruitment = ({
   };
 
   return (
-    <div
-      onClick={clickListItem}
-      className='w-[100%] flex items-center bg-yellow-600 px-[20px] py-[15px] relative cursor-pointer mb-[30px]'
-    >
-      <div className='font-bold text-[15px] ml-[20px]'>
-        <span className='text-xl'>{name}</span>
-        <div className='space-x-2 my-[2px]'>
-          <span>{meeting_type === 'offline' ? region : '온라인'}</span>
-          <span>{day}요일</span>
-        </div>
-        <div className='space-x-2'>
-          <span>{recruitment_status ? '모집 중' : '모집 완료'}</span>
-          <span>{time}</span>
-        </div>
-      </div>
-    </div>
+    <Container onClick={clickListItem}>
+      <GroupName>{name}</GroupName>
+      <GroupInformation>
+        <span>{meeting_type === 'offline' ? region : '온라인'}</span>
+        <Divider />
+        <span>매주 {day}</span>
+        <span>{time}</span>
+      </GroupInformation>
+      <GroupRrecruitmentButtonWrap className='space-x-2'>
+        <span></span>
+        <GroupRrecruitmentButton
+          recruitmentstatus={recruitment_status ? '모집중' : '모집완료'}
+        >
+          {recruitment_status ? '모집중' : '모집완료'}
+        </GroupRrecruitmentButton>
+      </GroupRrecruitmentButtonWrap>
+    </Container>
   );
 };
 
 export default ReadingGroupRecruitment;
+
+const Container = tw.div`
+  w-[100%]
+  p-[15px]
+  relative
+  cursor-pointer
+  mb-[30px]
+  border
+  border-black
+  border-opacity-10
+  rounded-lg	
+`;
+
+const GroupName = tw.span` 
+  font-bold
+  text-[17px]
+`;
+
+const GroupInformation = tw.div`
+  text-[13px]
+  text-black
+  text-opacity-50
+  space-x-1
+  my-[5px]
+  flex
+  items-center
+`;
+
+const Divider = tw.div`
+  w-[4px]
+  h-[4px]
+  bg-black
+  bg-opacity-30
+  border
+  rounded-full
+`;
+
+const GroupRrecruitmentButtonWrap = tw.div`
+  flex
+  justify-between
+`;
+
+const GroupRrecruitmentButton = tw.button<{ recruitmentstatus: string }>`
+  ${(props) => props.recruitmentstatus === '모집완료' && 'bg-opacity-50'}
+    
+  bg-[#6CA88D]
+  text-white
+  text-[10px]
+  rounded
+  px-[7.5px]
+  py-[5px]
+`;
--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import tw from 'tailwind-styled-components';
 
+import BottomNav from '@/components/common/BottomNav';
 import RecruitInfinityScrollLists from '@/components/recruit/RecruitInfinityScrollLists';
 
 const RecruitPage = () => {
@@ -12,23 +13,75 @@ const RecruitPage = () => {
   }, []);
 
   return (
-    <>
-      <h1>독서 인원 모집 페이지</h1>
-      {isMounted && <RecruitInfinityScrollLists />}
-      <PersonnelRecruitButton href='/recruit/write'>
-        독서 인원 모집 글 작성
-      </PersonnelRecruitButton>
-    </>
+    <Container>
+      <BackGroundWrap>
+        <BackGround>
+          <Title>
+            독서모임 시작하기 <br /> 참 좋은 하루에요!
+          </Title>
+        </BackGround>
+        <PersonnelRecruitButton href='/recruit/write'>
+          개설 하기
+        </PersonnelRecruitButton>
+      </BackGroundWrap>
+
+      <InfinityScrollListsWrap>
+        <InfinityScrollLists>
+          {isMounted && <RecruitInfinityScrollLists />}
+        </InfinityScrollLists>
+      </InfinityScrollListsWrap>
+
+      <BottomNav />
+    </Container>
   );
 };
 
 export default RecruitPage;
 
-const PersonnelRecruitButton = tw(Link)`
+const Container = tw.div``;
+
+const BackGroundWrap = tw.div`
   fixed
-  bottom-0
-  right-0
+  top-0
+  left-0
   w-full
-  bg-blue-500
+  h-[250px]
+  bg-[#FFFCEA]
+  z-10
+`;
+const BackGround = tw.div`
+  w-full
+  h-[250px]
+  relative
+`;
+
+const Title = tw.h1`
+  absolute
+  left-[20px]
+  bottom-[15px]
+  text-xl
+  font-bold
+`;
+
+const InfinityScrollListsWrap = tw.div`
+  pt-[290px]
+  pb-[60px]
+  bg-white
+`;
+
+const InfinityScrollLists = tw.div`
+  w-[90%]
+  mx-auto	
+`;
+
+const PersonnelRecruitButton = tw(Link)`
+  absolute
+  top-[70px]
+  right-[20px]
+  p-[5px]
+  bg-[#FFDF8D]
+  text-sm
   text-center
+  text-white
+  rounded
 `;
