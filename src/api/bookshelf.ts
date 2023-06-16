8cc46fc34e39da63c2ce1b7ae717925646e3f074
JaeHong Jeong <91203029+bohongu@users.noreply.github.com>
2023-06-16 17:41:45 +0900

    api: 내 책장 불러오기 (#182)

--- src/api/bookshelf.ts
+++ src/api/bookshelf.ts
@@ -2,6 +2,13 @@ import { bookshelfDataProps } from '@/types/bookshelf';
 
 import { axiosInstance } from './axios';
 
+interface BookshelfProps {
+  bookIsbn: string;
+  title: string;
+  thumbnail: string;
+  authors: string[];
+}
+
 // 책장에 책 추가
 export const postBookshelfApi = async (bookData: bookshelfDataProps) => {
   try {
@@ -14,5 +21,18 @@ export const postBookshelfApi = async (bookData: bookshelfDataProps) => {
     return data;
   } catch (error) {
     throw new Error('책을 추가할 수 없습니다.');
+  }
+};
+
+// 나의 책장 가져오기
+export const getMyBookShelfApi = async (): Promise<BookshelfProps[]> => {
+  try {
+    const { data } = await axiosInstance.request({
+      method: 'GET',
+      url: '/bookshelf',
+    });
+    return data;
+  } catch (error) {
+    throw new Error('책장을 가져올 수 없습니다.');
   }
 };
