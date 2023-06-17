3c3f3757b04b25fb1e5a32b5dd0620d84d1372c6
bohongu <jaehong2782@naver.com>
2023-06-17 23:49:50 +0900

    feat: 나의 책장, 다른 사람의 책장 구현

--- src/api/bookshelf.ts
+++ src/api/bookshelf.ts
@@ -24,12 +24,14 @@ export const postBookshelfApi = async (bookData: bookshelfDataProps) => {
   }
 };
 
-// 나의 책장 가져오기
-export const getMyBookShelfApi = async (): Promise<BookshelfProps[]> => {
+// 책장 가져오기
+export const getBookShelfApi = async (
+  ownerId?: string,
+): Promise<BookshelfProps[]> => {
   try {
     const { data } = await axiosInstance.request({
       method: 'GET',
-      url: '/bookshelf',
+      url: `${ownerId ? `/bookshelf/${ownerId}` : '/bookshelf'}`,
     });
     return data;
   } catch (error) {
