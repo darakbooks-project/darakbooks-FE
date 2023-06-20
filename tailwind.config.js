48384b2a66c9312c105b0df322f3a7da860ef054
유지영 <yj0zero@gmail.com>
2023-06-20 23:45:12 +0900

    fix: 메인 컬러 변수 재변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        main: '#67A68A',
+        main: '#60B28D',
         background: '#FFFEF8',
       },
       boxShadow: {
