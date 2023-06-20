79c6165c56a8321fd1cb06e17b7bd0f5794d26bb
유지영 <yj0zero@gmail.com>
2023-06-20 23:38:40 +0900

    feat: 메인 페이지 임시 추천 페이지 이동 버튼 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import BookShelfPreview from '@/components/common/BookShelfPreview';
 import BottomNav from '@/components/common/BottomNav';
 import BestRecruitList from '@/components/main/bestRecruit/BestRecruitList';
 import RecordFeedList from '@/components/main/mainRecordFeed/RecordFeedList';
+import { useAuth } from '@/hooks/useAuth';
 import { isAuthorizedSelector } from '@/recoil/auth';
 import { BestGroupListType } from '@/types/recruit';
 
@@ -26,11 +27,18 @@ export default function Home({
 }: {
   bestGroup: BestGroupListType[];
 }) {
+  const { openAuthRequiredModal } = useAuth();
   const isAuthorized = useRecoilValue(isAuthorizedSelector);
   const {
     query: { isRendedOnboarding },
     push,
   } = useRouter();
+
+  const handleMoveRecommendPage = () => {
+    if (!isAuthorized) return openAuthRequiredModal();
+
+    push('/recommend');
+  };
 
   if (!isAuthorized && !isRendedOnboarding) {
     push('/onboarding');
@@ -45,21 +53,24 @@ export default function Home({
           <p className='text-base'>오늘은 어떤 책을 읽으셨나요?</p>
         </div>
       </section>
-      <section className='mt-14'>
-        <p className='mx-5 text-sm font-bold text-main'>
+      <section className='mx-5 mt-14'>
+        <p className='text-sm font-bold text-main'>
           오늘의 나를 위한 도서 선택
         </p>
-        <h1 className='mx-5 mb-5 text-xl font-bold'>인기서재 추천</h1>
-        <div className='mx-5'>
-          <BookShelfPreview
-            key={BOOKSHELFDUMMY.memberId}
-            nickname={BOOKSHELFDUMMY.nickname}
-            imageSrcArr={BOOKSHELFDUMMY.images}
-            memberId='2823683088'
-          />
-        </div>
+        <h1 className='mb-5 text-xl font-bold'>인기서재 추천</h1>
+        <BookShelfPreview
+          key={BOOKSHELFDUMMY.memberId}
+          nickname={BOOKSHELFDUMMY.nickname}
+          imageSrcArr={BOOKSHELFDUMMY.images}
+          memberId='2823683088'
+        />
       </section>
       <BestRecruitList BestGroupList={bestGroup} />
+      <section>
+        <div onClick={handleMoveRecommendPage} className='mx-5 bg-white'>
+          추천 페이지로 이동
+        </div>
+      </section>
       <section className='mt-10'>
         <p className='mx-5 text-sm font-bold text-main'>
           요즘 푹 빠져있는 관심사
