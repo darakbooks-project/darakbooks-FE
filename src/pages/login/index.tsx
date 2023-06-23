e62494761ab115b2a5ccfa993eeef917b8018963
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-06-23 14:35:32 +0900

    fix: 로그인한 유저는 로그인 페이지 접근 금지

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,51 +1,64 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 import tw from 'tailwind-styled-components';
 
 import LoginButton from '@/components/auth/LoginButton';
+import { isAuthorizedSelector } from '@/recoil/auth';
 
 const LoginPage = () => {
+  const { back } = useRouter();
+  const isAuthorized = useRecoilValue(isAuthorizedSelector);
+
+  useEffect(() => {
+    if (isAuthorized) back();
+  }, [isAuthorized, back]);
+
   return (
-    <Container>
-      <Wrap>
-        <Introduce>
-          <SiteDescrtion className='vertical-text'>
-            나만의
-            <span className='my-3' />
-            작은책방
-          </SiteDescrtion>
-          <SiteName className='vertical-text'>다락책방</SiteName>
-          {/* by 민형, 일러스트 넣기_230622 */}
-          {/* <SiteImage
+    !isAuthorized && (
+      <Container>
+        <Wrap>
+          <Introduce>
+            <SiteDescrtion className='vertical-text'>
+              나만의
+              <span className='my-3' />
+              작은책방
+            </SiteDescrtion>
+            <SiteName className='vertical-text'>다락책방</SiteName>
+            {/* by 민형, 일러스트 넣기_230622 */}
+            {/* <SiteImage
             src='./images/login-image.svg'
             width={200}
             height={400}
             alt='로그인 페이지 사진'
           /> */}
-        </Introduce>
+          </Introduce>
 
-        <UserSelectWrapper>
-          <QuestionText>
-            3초만에 가입하고 <br /> 다락책방을 시작해볼까요?
-          </QuestionText>
-          <LoginButton>
-            <ButtonItem>
-              <Image
-                src='./images/kakao-logo.svg'
-                width={17.5}
-                height={17.5}
-                alt='카카오 로고 사진'
-              />
-            </ButtonItem>
-            <ButtonItem className='text-center w-[50%]'>
-              카카오로 시작하기
-            </ButtonItem>
-            <ButtonItem></ButtonItem>
-          </LoginButton>
-          <LinkStyles href='/'>일단 둘러보기</LinkStyles>
-        </UserSelectWrapper>
-      </Wrap>
-    </Container>
+          <UserSelectWrapper>
+            <QuestionText>
+              3초만에 가입하고 <br /> 다락책방을 시작해볼까요?
+            </QuestionText>
+            <LoginButton>
+              <ButtonItem>
+                <Image
+                  src='./images/kakao-logo.svg'
+                  width={17.5}
+                  height={17.5}
+                  alt='카카오 로고 사진'
+                />
+              </ButtonItem>
+              <ButtonItem className='text-center w-[50%]'>
+                카카오로 시작하기
+              </ButtonItem>
+              <ButtonItem></ButtonItem>
+            </LoginButton>
+            <LinkStyles href='/'>일단 둘러보기</LinkStyles>
+          </UserSelectWrapper>
+        </Wrap>
+      </Container>
+    )
   );
 };
 
