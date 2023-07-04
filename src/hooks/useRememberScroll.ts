33511716628e8429fcb58d63259cb3ad94e51e52
유지영 <yj0zero@gmail.com>
2023-07-04 18:45:11 +0900

    feat: 스크롤 위치 기억하는 커스텀 훅 구현

---
+++ src/hooks/useRememberScroll.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const useRememberScroll = (key: string) => {
+  const [currentScroll, setCurrentScroll] = useState(() => {
+    const saveScroll = localStorage.getItem(key);
+    return saveScroll ? JSON.parse(saveScroll) : '0';
+  });
+
+  const setScroll = () => {
+    const currentY = window.scrollY;
+    setCurrentScroll(currentY);
+    localStorage.setItem(key, JSON.stringify(currentY));
+  };
+
+  const resetScroll = () => {
+    setCurrentScroll('0');
+    localStorage.removeItem(key);
+  };
+  return { currentScroll, setScroll, resetScroll };
+};
+
+export default useRememberScroll;
