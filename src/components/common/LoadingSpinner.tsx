ca57811f006987f97fd669f174ff1357d6a1461a
유지영 <yj0zero@gmail.com>
2023-07-04 10:54:24 +0900

    design: 로딩 시 body 스크롤 막기

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,6 +1,16 @@
 import Image from 'next/image';
+import { useEffect } from 'react';
+
+import { allowScroll, blockScroll } from '@/utils/changeBodyScroll';
 
 export const LoadingSpinner = () => {
+  useEffect(() => {
+    const prevScrollY = blockScroll();
+    return () => {
+      allowScroll(prevScrollY);
+    };
+  }, []);
+
   return (
     <div className='w-full h-full max-w-xl fixed mx-auto left-0 top-0 bottom-0 right-0 z-40 flex justify-center items-center overflow-hidden'>
       <Image src='/images/loading.svg' width={80} height={80} alt='로딩' />
