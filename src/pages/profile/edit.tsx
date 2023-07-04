addbe60fbeb3d498b76074cf222f42af48f75d52
jaehong <jaehong2782@gmail.com>
2023-07-04 12:04:00 +0900

    feat: 공개범위 클릭 시 안내문구 색변화, 안내문구 수정

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -171,8 +171,14 @@ const Edit = () => {
                         공개
                       </span>
                     </div>
-                    <p className='not-italic font-normal text-[11px] text-[#999797]'>
-                      나의 모든 게시글, 책장, 기록 전체 공개
+                    <p
+                      className={
+                        secretMode === 'PUBLIC'
+                          ? 'font-normal text-[11px] text-[#60B28D]'
+                          : 'font-normal text-[11px] text-[#999797]'
+                      }
+                    >
+                      마이페이지 공개
                     </p>
                   </article>
                   <article className='flex items-center justify-between mb-2'>
@@ -188,8 +194,14 @@ const Edit = () => {
                         비공개
                       </span>
                     </div>
-                    <p className='not-italic font-normal text-[11px] text-[#999797]'>
-                      나의 모든 게시글, 책장, 기록 전체 비공개
+                    <p
+                      className={
+                        secretMode === 'PRIVATE'
+                          ? 'font-normal text-[11px] text-[#60B28D]'
+                          : 'font-normal text-[11px] text-[#999797]'
+                      }
+                    >
+                      마이페이지 비공개
                     </p>
                   </article>
                 </div>
