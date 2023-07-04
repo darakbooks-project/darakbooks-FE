85538c146a865a3933406c3be7242a3957496aa9
유지영 <yj0zero@gmail.com>
2023-07-04 15:47:24 +0900

    refactor: 로딩 시 스크롤 막기 overflow hidden으로 변경

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,18 +1,16 @@
 import Image from 'next/image';
 import { useEffect } from 'react';
 
-import { allowScroll, blockScroll } from '@/utils/changeBodyScroll';
-
 export const LoadingSpinner = () => {
   useEffect(() => {
-    const prevScrollY = blockScroll();
+    document.body.style.overflow = 'hidden';
     return () => {
-      allowScroll(prevScrollY);
+      document.body.style.overflow = 'initial';
     };
   }, []);
 
   return (
-    <div className='w-full h-full max-w-xl fixed mx-auto left-0 top-0 bottom-0 right-0 z-40 flex justify-center items-center overflow-hidden'>
+    <div className='w-full h-full max-w-xl fixed mx-auto top-0 bottom-0 z-40 flex justify-center items-center overflow-hidden'>
       <Image src='/images/loading.svg' width={80} height={80} alt='로딩' />
     </div>
   );
