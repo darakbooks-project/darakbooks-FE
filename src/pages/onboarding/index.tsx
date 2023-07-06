77f3bcada4042041324b0a79c34cabb00043b9cf
JiYoung <yj0zero@gmail.com>
2023-07-06 13:30:15 +0900

    feat: 스킵 버튼 클릭 시 마지막 슬라이드로 이동 (#370)
    * refactor: 스킵버튼 클릭 시 마지막 슬라이드로 이동되도록 수정
    
    * design: 폰트 및 폰트 크기 조절

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React, { Fragment, useState } from 'react';
-import Slider from 'react-slick';
+import React, { Fragment, useRef, useState } from 'react';
+import Slider, { Settings } from 'react-slick';
 import { useSetRecoilState } from 'recoil';
 
 import { isRendedOnboardingAtom } from '@/recoil/onboarding';
@@ -10,30 +10,30 @@ const ONBOADINGDATA = [
   {
     title: '책으로 채우는 일상',
     desc: (
-      <h3>
+      <>
         오늘은 무슨 책을 읽으셨나요?
         <br /> 좋아하는 책, 인상깊은 구절을 기록해보세요
-      </h3>
+      </>
     ),
     image: '/images/onboarding/onboarding1.svg',
   },
   {
     title: '함께하는 지적 대화',
     desc: (
-      <h3>
+      <>
         누군가와 같은 책을 읽고 함께 하는 이야기, <br />
         비슷한 취향의 사람들과 함께 소통해보세요
-      </h3>
+      </>
     ),
     image: '/images/onboarding/onboarding2.svg',
   },
   {
     title: '더많은 책과 친해지기',
     desc: (
-      <h3>
+      <>
         수많은 책들 사이 나에게 딱! 맞는 책 한 권<br />
         다락책방이 차근차근 골라드릴게요
-      </h3>
+      </>
     ),
     image: '/images/onboarding/onboarding3.svg',
   },
@@ -43,8 +43,9 @@ const OnboardingPage = () => {
   const [lastSlide, setLastSlide] = useState(false);
   const setIsRendedOnboarding = useSetRecoilState(isRendedOnboardingAtom);
   const router = useRouter();
+  const sliderRef = useRef<Slider>(null);
 
-  const settings = {
+  const settings: Settings = {
     dots: !lastSlide,
     dotsClass: 'dots_custom',
     infinite: true,
@@ -66,14 +67,18 @@ const OnboardingPage = () => {
     router.push('/recommend/intro', '/recommend');
   };
 
+  const handleMoveSkip = () => {
+    sliderRef.current?.slickGoTo(2);
+  };
+
   return (
     <div className='min-h-[100%] bg-background pb-4'>
-      <div className='relative'>
-        <Slider {...settings}>
+      <div className='relative w-5/6 mx-auto'>
+        <Slider {...settings} ref={sliderRef}>
           {ONBOADINGDATA.map((item) => (
             <Fragment key={item.title}>
-              <div className='flex flex-col justify-center w-5/6 pb-11'>
-                <h2 className='mb-3 text-2xl font-bold text-main xxs:text-xl'>
+              <div className='flex flex-col justify-center w-full pb-11'>
+                <h2 className='mb-3 text-[1.625rem] font-medium text-main xxs:text-xl'>
                   {item.title}
                 </h2>
                 <p className='text-[#707070] xxs:text-sm'>{item.desc}</p>
@@ -84,19 +89,19 @@ const OnboardingPage = () => {
                 width={350}
                 height={350}
                 alt='온보딩 이미지'
-                className='w-5/6 max-h-96'
+                className='max-h-96'
               />
               {lastSlide && (
                 <>
                   <button
                     onClick={handleMoveRecommend}
-                    className='flex items-center justify-center w-5/6 my-3 border-2 rounded-lg h-14 border-main text-main'
+                    className='flex items-center justify-center w-full my-3 border-2 rounded-lg h-14 border-main text-main'
                   >
                     도서 추천 받기
                   </button>
                   <button
                     onClick={handleMoveMain}
-                    className='w-5/6 text-white border-2 rounded-lg h-14 border-main bg-main'
+                    className='w-full text-white border-2 rounded-lg h-14 border-main bg-main'
                   >
                     다락책방 들어가기
                   </button>
@@ -106,12 +111,14 @@ const OnboardingPage = () => {
           ))}
         </Slider>
         {!lastSlide && (
-          <button
-            onClick={handleMoveMain}
-            className='absolute w-5/6 pt-4 pb-0 pr-3 text-right bottom-2 left-[10%] text-[#999797]'
-          >
-            Skip
-          </button>
+          <div className='absolute right-0 flex flex-row-reverse bottom-2'>
+            <button
+              onClick={handleMoveSkip}
+              className='pt-4 pb-0 pr-3 text-[#999797] font-prettyNight'
+            >
+              Skip
+            </button>
+          </div>
         )}
       </div>
     </div>
