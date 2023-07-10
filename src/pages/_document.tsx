979d9b9aa9cdfb69a9bfd1079a4f0859142a8f5f
jaehong <jaehong2782@gmail.com>
2023-07-10 15:16:01 +0900

    feat: favicon 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='ko'>
-      <Head />
+      <Head>
+        <link rel='icon' href='/favicon.svg' />
+      </Head>
       <body>
         <Main />
         <div id='portal' />
