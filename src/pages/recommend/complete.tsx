ad0672e1252d7783d5a724d8ff594e1520e64a43
유지영 <yj0zero@gmail.com>
2023-07-10 21:02:10 +0900

    design: 추천페이지 - 결과 디자인 변경사항 적용

--- src/pages/recommend/complete.tsx
+++ src/pages/recommend/complete.tsx
@@ -1,22 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { getBookDataByTitle } from '@/api/book';
-import Header from '@/components/common/Header';
-import RecommendResultModal from '@/components/recommend/RecommendResultModal';
-import { selectRecommendResultModalAtom } from '@/recoil/modal';
+import { postBookshelfApi } from '@/api/bookshelf';
+import { isAuthorizedSelector } from '@/recoil/auth';
+import { isRendedOnboardingAtom } from '@/recoil/onboarding';
 import { RecommendBookResult } from '@/recoil/recommend';
 
 const RecommendCompletePage = () => {
-  const {
-    title: bookTitle,
-    reason,
-    userRequest,
-  } = useRecoilValue(RecommendBookResult);
-  const [modal, setModal] = useRecoilState(selectRecommendResultModalAtom);
+  const { title: bookTitle, reason } = useRecoilValue(RecommendBookResult);
   const router = useRouter();
+  const { mutate: postMyBookshelf } = useMutation(postBookshelfApi);
+  const isAuthorized = useRecoilValue(isAuthorizedSelector);
+  const setIsRendedOnboarding = useSetRecoilState(isRendedOnboardingAtom);
 
   const {
     data: recommendBookData,
@@ -31,10 +31,11 @@ const RecommendCompletePage = () => {
 
   if (isError || !recommendBookData.documents.length)
     return (
-      <div className='flex flex-col items-center justify-center h-full'>
-        <p>책을 찾지 못했어요! 다시 시도해주세요</p>
+      <div className='flex flex-col items-center justify-center h-full bg-[#FFFEF8] text-textBlack'>
+        <h3 className='font-medium text-clamp2xl'>책을 찾지 못했어요!</h3>
+        <p>다시 시도해주세요😥</p>
         <button
-          className='text-white bg-main'
+          className='p-2 mt-4 text-white rounded bg-main'
           onClick={() => {
             router.back();
           }}
@@ -46,42 +47,98 @@ const RecommendCompletePage = () => {
 
   const { authors, isbn, thumbnail, title } = recommendBookData.documents[0];
 
-  const bookData = {
-    authors,
-    isbn: isbn.split(' ')[0],
-    thumbnail,
-    title,
-    reason,
+  const handleAddMyBookShelf = () => {
+    const bookData = {
+      title,
+      bookIsbn: isbn.split(' ')[0],
+      thumbnail,
+      authors,
+    };
+    postMyBookshelf(bookData, {
+      onSuccess: () => {
+        alert('내 책장에 책을 담았습니다.');
+        router.push('/profile');
+      },
+      onError: (error) => {
+        const { status } = error as AxiosError;
+        if (status === 403) {
+          alert('이미 책장에 저장된 책입니다.');
+        }
+      },
+    });
+  };
+
+  const handleMoveBookDetailPage = () => {
+    router.push(`/book/detail?isbn=${isbn.split(' ')[0]}`);
+  };
+
+  const handleMoveMain = () => {
+    router.push('/');
+    setIsRendedOnboarding(true);
+  };
+
+  const changeBookButton = (isAuthorized: boolean): React.ReactNode => {
+    const loginUserButton = (
+      <div className='flex justify-center mt-7'>
+        <button
+          onClick={handleAddMyBookShelf}
+          className='flex items-center justify-center w-2/5 mr-3 font-medium bg-white border-2 rounded-lg h-14 border-main shadow-round text-main'
+        >
+          <Image
+            src='/images/bookRecommend/bookshelfIcon.svg'
+            width={32}
+            height={32}
+            alt='책장 아이콘'
+          />
+          담기
+        </button>
+        <button
+          onClick={handleMoveBookDetailPage}
+          className='w-3/5 font-medium text-white rounded-lg h-14 bg-main shadow-round'
+        >
+          책 상세보기
+        </button>
+      </div>
+    );
+
+    const guestButton = (
+      <button
+        onClick={handleMoveMain}
+        className='w-full font-medium text-white rounded-lg h-14 mt-7 bg-main shadow-round'
+      >
+        책방 들어가기
+      </button>
+    );
+
+    return isAuthorized ? loginUserButton : guestButton;
   };
 
   return (
-    <div className='flex flex-col items-center justify-between h-full px-8 bg-[url(/images/bookRecommend/background3.svg)] bg-no-repeat bg-cover bg-center'>
-      <Header className='flex-initial pl-0 mt-14' />
-      <div className='flex-initial w-full'>
-        <h2 className='mb-5 font-semibold text-center text-clampLg'>
-          나의 답변
-        </h2>
-        <p className='w-full text-center border-b'>
-          &quot;{userRequest}&ldquo;
-        </p>
+    <div className='h-full bg-[url(/images/bookRecommend/background3.svg)] bg-no-repeat bg-cover bg-center'>
+      <div className='flex items-center justify-center h-[482px]'>
+        <Image
+          src={thumbnail}
+          width={187}
+          height={284}
+          placeholder='blur'
+          blurDataURL={thumbnail}
+          alt='추천 책 표지'
+          className='max-w-[11.6875rem] h-full max-h-[17.75rem] w-full rounded-r-md xs:px-1 drop-shadow-around xxs:w-[160px] xxs:h-[15rem]'
+        />
       </div>
-      <div className='flex-[6_6_300px] w-full flex items-center justify-center'>
-        <div className='relative w-5/6 h-4/6 bg-white border-dashed border border-[#707070] rounded-2xl flex justify-center items-center flex-col'>
-          <p className='text-6xl text-[#C2C1C180] font-bold mb-4 text-center'>
-            ?
-          </p>
-          <p className='text-clampXl text-[#333333] font-semibold'>
-            나에게 맞는 책은
-          </p>
+      <div className='absolute bottom-0 top-[482px] w-full bg-white shadow-roundY min-h-[22.625rem] rounded-t-[1.875rem] max-w-xl'>
+        <div className='flex flex-col items-center px-11 mb-14'>
+          <h2 className='pt-8 pb-1 font-bold text-clamp2xl text-main'>
+            {title}
+          </h2>
+          <h3 className='text-[#707070] text-clampBase'>{authors}</h3>
+          <div className='h-1 w-7 bg-[#60B28D80] my-5 ' />
+          <p className=''>{reason}</p>
+        </div>
+        <div className='absolute w-full px-5 bottom-10'>
+          {changeBookButton(isAuthorized)}
         </div>
       </div>
-      <button
-        onClick={() => setModal(true)}
-        className='w-full flex-initial text-white rounded-lg h-14 bg-main disabled:bg-[#DFDFDF] mb-10'
-      >
-        확인하기
-      </button>
-      {modal && <RecommendResultModal bookData={bookData} />}
     </div>
   );
 };
--- src/recoil/recommend.ts
+++ src/recoil/recommend.ts
@@ -2,14 +2,9 @@ import { atom } from 'recoil';
 
 import { RecommendBookResultType } from '@/types/recommend';
 
-interface RecommendBookAtomType extends RecommendBookResultType {
-  userRequest: string;
-}
-
-export const RecommendBookResult = atom<RecommendBookAtomType>({
+export const RecommendBookResult = atom<RecommendBookResultType>({
   key: 'RecommendBookKey',
   default: {
-    userRequest: '',
     reason: '',
     title: '',
   },
