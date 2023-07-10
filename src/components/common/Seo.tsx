e71757a6993ba8ab1989946ce235efb51366467e
jaehong <jaehong2782@gmail.com>
2023-07-10 14:54:15 +0900

    feat: Seo 컴포넌트 수정

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -26,13 +26,13 @@ function Seo({ title, description, url, image }: SeoProps) {
       <meta property='og:type' content='website' />
       <meta
         property='og:url'
-        content={'frontend-book-platform.vercel.app/' || url}
+        content={url || 'frontend-book-platform.vercel.app/'}
       />
       <meta
         property='og:image'
         content={
-          '로컬 이미지를 웹 페이지에 추가하려면 이미지를 웹 서버에 업로드하고 해당 이미지의 URL을 meta 태그의 content 속성에 지정해야 합니다.' ||
-          image
+          image ||
+          '로컬 이미지를 웹 페이지에 추가하려면 이미지를 웹 서버에 업로드하고 해당 이미지의 URL을 meta 태그의 content 속성에 지정해야 합니다.'
         }
       />
       <meta
