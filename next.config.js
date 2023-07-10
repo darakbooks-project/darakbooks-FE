f9b182447c0f4124aa01bf1689614d4e310b178e
유지영 <yj0zero@gmail.com>
2023-07-10 19:28:01 +0900

    chore: bundle-analyzer config 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -25,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
