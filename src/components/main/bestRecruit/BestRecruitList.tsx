a75cc2ac94ab3bcdf5f632d0f1be39c82b2c5e32
유지영 <yj0zero@gmail.com>
2023-07-16 22:01:59 +0900

    design: 인기 모임 없을 때 처리

--- src/components/main/bestRecruit/BestRecruitList.tsx
+++ src/components/main/bestRecruit/BestRecruitList.tsx
@@ -11,17 +11,28 @@ const BestRecruitList = ({
 }) => {
   return (
     <section className='bg-white border border-solid border-[#DFDFDF] rounded-md mx-5 mt-14'>
-      <h2 className=' mx-5 my-7 text-clampLg font-bold'>
+      <h2 className='mx-5 font-bold my-7 text-clampLg'>
         지금 가장 인기있는 모임
       </h2>
       <ul>
-        {BestGroupList.map((group, i) => (
-          <BestRecruitListItem
-            key={group.group_group_id}
-            {...group}
-            index={i}
-          />
-        ))}
+        {BestGroupList.length ? (
+          BestGroupList.map((group, i) => (
+            <BestRecruitListItem
+              key={group.group_group_id}
+              {...group}
+              index={i}
+            />
+          ))
+        ) : (
+          <div className='flex flex-col items-center justify-center m-6 text-center'>
+            <h4 className='font-medium text-clampBase'>
+              생성된 모임이 없어요.
+            </h4>
+            <p className=' text-clampSm text-[#707070] pt-1'>
+              지금 독서 모임을 생성해보세요!
+            </p>
+          </div>
+        )}
       </ul>
     </section>
   );
