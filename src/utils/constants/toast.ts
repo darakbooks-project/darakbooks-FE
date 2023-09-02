33fa46cdb54d0a5ad9a2d42cb1173bbe39408af1
유지영 <yj0zero@gmail.com>
2023-09-02 19:42:51 +0900

    feat: toast ui 관련 기본 타입 및 스타일 설정

---
+++ src/utils/constants/toast.ts
@@ -0,0 +1,19 @@
+export const toastType = {
+  error: {
+    backgroundColor: '#FCDDDD',
+    fontColor: '#F05050',
+    icon: '/images/toast/error.svg',
+  },
+
+  success: {
+    backgroundColor: '#E1F5EB',
+    fontColor: '#60B28D',
+    icon: '/images/toast/success.svg',
+  },
+
+  warning: {
+    backgroundColor: '#FCF4DE',
+    fontColor: '#fab005',
+    icon: '/images/toast/warning.svg',
+  },
+};
